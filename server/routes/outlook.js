a1484632a3357f0da1fd965f286d921575dec590
David Stone <davidjamesstone@gmail.com>
2018-11-15 10:29:38 +0000

    National view

--- server/routes/outlook.js
+++ server/routes/outlook.js
@@ -5,8 +5,8 @@ module.exports = {
   method: 'GET',
   path: '/outlook',
   handler: async (request, h) => {
-    const outlook = await floodService.getOutlook()
-    const model = new ViewModel(outlook)
+    const { statement: outlook } = await floodService.getOutlook()
+    const model = new ViewModel({ outlook })
 
     return h.view('outlook', { model })
   }
