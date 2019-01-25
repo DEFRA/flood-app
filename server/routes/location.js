c0a482fab2833d57c6446824f8d0d041f1460005
John Shields <John.Shields@environment-agency.gov.uk>
2019-01-25 19:34:45 +0000

    FLO-3334/3355: Test for invalid and non-England locations.

--- server/routes/location.js
+++ server/routes/location.js
@@ -9,6 +9,13 @@ module.exports = {
   handler: async (request, h) => {
     const { q: location } = request.query
     const place = await locationService.find(location)
+    if (typeof place === 'undefined') {
+      request.yar.set('displayError', { errorMessage: 'No results match your search term. Please try again.' })
+      return h.redirect('/')
+    }
+    if (!place.isEngland.is_england) {
+      return h.redirect('/location-not-england')
+    }
     const { floods } = await floodService.getFloodsWithin(place.bbox)
     const stations = await floodService.getStationsWithin(place.bbox)
     const model = new ViewModel({ place, floods, stations })
