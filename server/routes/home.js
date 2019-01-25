86828fe36a87fa57de4962794b79eb503bddf58e
John Shields <John.Shields@environment-agency.gov.uk>
2019-01-25 19:31:18 +0000

    FLO-3332/3355: Display error messages for blank or invalid locations entered.

--- server/routes/home.js
+++ server/routes/home.js
@@ -3,24 +3,27 @@ const joi = require('joi')
 module.exports = [{
   method: 'GET',
   path: '/',
-  handler: {
-    view: 'home'
+  handler: async (request, h) => {
+    if (typeof request.yar.get('displayError') === 'undefined') {
+      return h.view('home')
+    } else {
+      return h.view('home', request.yar.get('displayError'))
+    }
   }
 }, {
   method: 'POST',
   path: '/',
   handler: async (request, h) => {
     const { location } = request.payload
-    return h.redirect(!location ? '/national' : `/location?q=${location}`)
+    return h.redirect(`/location?q=${location}`)
   },
   options: {
     validate: {
       payload: {
-        // location: joi.string().required().allow('')
         location: joi.string().required()
       },
       failAction: (request, h, err) => {
-        return h.view('home', { errorMessage: 'Location empty' }).takeover()
+        return h.view('home', { errorMessage: 'Please enter a location' }).takeover()
       }
     }
   }
