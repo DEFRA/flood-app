e33412ce5c70063c79de38bce4ceed6f213bd260
danleech-defra <danleech.defra@gmail.com>
2019-02-15 13:24:53 +0000

    Initial rain hauge bar chart

--- server/models/views/rain-gauge.js
+++ server/models/views/rain-gauge.js
@@ -1,143 +1,52 @@
 const moment = require('moment-timezone')
 
 // Generate some data
-function data () {
-  var startTime = moment('2019-02-18T09:00:00Z')
-  var values = [0, 0, 1, 2, 2, 2, 2, 2, 3, 3, 1, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0]
-  var fifteen = []
-  for (var i = 0; i <= values.length; i++) {
-    // startTime.add(15, 'minutes')
-    console.log('index ' + i)
-    /*
-    console.log({
-      'index ': i
-    })
-    /*
-    var dateTime = startTime.add(15, 'minutes')
-    fifteen.push({
-      'timestamp': dateTime.format(),
-      'period': dateTime.format('HH:mm'),
-      'value': value + 'mm'
-    })
-    console.log({
-      'index': i,
-      'timestamp': dateTime.format(),
-      'period': dateTime.format('HH:mm'),
-      'value': values[i] + 'mm'
-    })
-    */
+var startTime = moment('2019-02-18T09:00:00Z')
+var values = [0, 0, 1, 2, 2, 2, 2, 2, 3, 3, 1, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0]
+var fifteen = []
+// Generate vertical axis values
+var vMaxValue = 5 // Vertical axis max
+if (Math.max.apply(Math, values) > vMaxValue) {
+  vMaxValue = Math.max.apply(Math, values) + 1
+}
+var vTickValue = Math.ceil((vMaxValue + 1) / 6) // Vertical tick value
+var vTicksCount = Math.floor(vMaxValue/vTickValue) // Number of vertical ticks
+var vTickPercentile = ((100/vMaxValue)*vTickValue).toFixed(2) // Percentage spacing of each tick
+// H axis config
+var hTickFirstOffset = 2
+var hTickIncrement = 3
+var hTickPercentile = (100/24).toFixed(2)
+// Generate records
+for (var i = 0; i < 24; i++) {
+  var dateTime = startTime.add(15, 'minutes')
+  var column = {
+    'timestamp': dateTime.format(),
+    'period': dateTime.format('HH:mm'),
+    'value': values[i],
+    'vPercentile': ((100/vMaxValue)*values[i]).toFixed(2),
+    'hPercentile': (i * hTickPercentile),
+    'hWidthPercentile': hTickPercentile,
+    'isTick': false
   }
-  return startTime
+  if ((i+hTickFirstOffset) % hTickIncrement === 0) {
+    // THis is also an hTick tick
+    column.isTick = true 
+  }
+  fifteen.push(column)
 }
 
 class ViewModel {
   constructor (name) {
-    data()
     this.name = 'Monksilver'
     this.gridRef = 'SS763417'
     this.coordinates = [-3.77, 51.16]
-    this.fifteen = [
-      {
-        'dateTime': '2019-02-18T09:00:00Z',
-        'period': '9:00',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T09:15:00Z',
-        'period': '9:15',
-        'value': 1
-      },
-      {
-        'dateTime': '2019-02-18T09:30:00Z',
-        'period': '9:30',
-        'value': 2
-      },
-      {
-        'dateTime': '2019-02-18T09:45:00Z',
-        'period': '9:15',
-        'value': 2
-      },
-      {
-        'dateTime': '2019-02-18T10:00:00Z',
-        'value': 3
-      },
-      {
-        'dateTime': '2019-02-18T10:15:00Z',
-        'value': 1
-      },
-      {
-        'dateTime': '2019-02-18T10:30:00Z',
-        'value': 1
-      },
-      {
-        'dateTime': '2019-02-18T10:45:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T11:00:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T11:15:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T11:30:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T11:45:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T12:00:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T12:15:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T12:30:00Z',
-        'value': 2
-      },
-      {
-        'dateTime': '2019-02-18T12:45:00Z',
-        'value': 3
-      },
-      {
-        'dateTime': '2019-02-18T13:00:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T13:15:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T13:30:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T13:45:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T14:00:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T14:15:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T14:30:00Z',
-        'value': 0
-      },
-      {
-        'dateTime': '2019-02-18T14:45:00Z',
-        'value': 0
-      }
-    ]
+    this.fifteen = fifteen
+    this.vAxisFifteen = {
+      'vMaxValue': vMaxValue,
+      'vTickValue': vTickValue,
+      'vTicksCount': vTicksCount + 1, // Added for 0 tick
+      'vTickPercentile': vTickPercentile
+    }
   }
 }
 
