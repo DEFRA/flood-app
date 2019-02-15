6d4341320a82da5c688634bdd6bf220d821ef602
danleech-defra <danleech.defra@gmail.com>
2019-02-15 15:54:40 +0000

    Horizontal start time fixed

--- server/models/views/rain-gauge.js
+++ server/models/views/rain-gauge.js
@@ -1,41 +1,48 @@
 const moment = require('moment-timezone')
 
 // Generate some data
-var startTime = moment('2019-02-18T15:15:00Z')
+var startTime = moment('2019-02-18T15:00:00Z')
 var values = [0, 0, 1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0]
 var fifteenThreshold = -1
+
 // Axis config
 var verticalAxisMax = 10
 var hTickFirstOffset = 2
 var hTickIncrement = 3
 var hTickPercentile = (100/24).toFixed(2)
+
 // Generate vertical axis values
 var vMaxValue = Math.max.apply(Math, values) + 1 // Vertical axis max
+
 // Set vertical axis to fixed upper value
 if (vMaxValue < verticalAxisMax) {
   vMaxValue = verticalAxisMax
 }
+
 // Extend vertical axis for threshold
 if (vMaxValue < fifteenThreshold) {
   vMaxValue = fifteenThreshold + 1
 }
-// Calculate tick intervals and counts
+
+// Calculate tick values and counts
 var vTickValue = Math.ceil((vMaxValue + 1) / 6) // Vertical tick value
 var vTicksCount = Math.floor(vMaxValue/vTickValue) // Number of vertical ticks
+
 // Extend vertical axis if max value does not fall on a tick
 if (vTickValue * vTicksCount < vMaxValue) {
   vTicksCount ++
   vMaxValue = vTicksCount * vTickValue
 }
+
 // Calculate percentage spacing for each tick
 var vTickPercentile = ((100/vMaxValue)*vTickValue).toFixed(2)
+
 // Generate records
 var fifteen = []
 for (var i = 0; i < 24; i++) {
-  var dateTime = startTime.add(15, 'minutes')
   var column = {
-    'timestamp': dateTime.format(),
-    'period': dateTime.format('HH:mm'),
+    'timestamp': startTime.format(),
+    'period': startTime.format('HH:mm'),
     'value': values[i],
     'vPercentile': ((100/vMaxValue)*values[i]).toFixed(2),
     'hPercentile': (i * hTickPercentile),
@@ -47,6 +54,7 @@ for (var i = 0; i < 24; i++) {
     column.isTick = true 
   }
   fifteen.push(column)
+  startTime.add(15, 'minutes')
 }
 
 class ViewModel {
