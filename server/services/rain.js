cecf6108fbd4d0b5e5edd67f21ee27da4a216a13
John Shields <John.Shields@environment-agency.gov.uk>
2019-03-01 11:54:50 +0000

    FLO-3380: Refactor async funtion declaration.

--- server/services/rain.js
+++ server/services/rain.js
@@ -1,13 +1,13 @@
 const util = require('../util')
 
 module.exports = {
-  async getRainGaugeById (id) {
+  getRainGaugeById: async (id) => {
     const url = `https://environment.data.gov.uk/flood-monitoring/id/stations/${id}`
     const result = await util.getJson(url, true)
     return result
   },
-  async getRainMeasuresById (id) {
-    const url = `https://environment.data.gov.uk/flood-monitoring/id/stations/${id}/readings?_limit=2800&_sorted`
+  getRainMeasuresById: async (id) => {
+    const url = `http://environment.data.gov.uk/flood-monitoring/id/stations/${id}/readings?parameter=rainfall&_sorted&_limit=2800&_sorted`
     const result = await util.getJson(url, true)
     return result
   }
