05f96e73e287fc529a843979176395c4c2965426
John Shields <John.Shields@environment-agency.gov.uk>
2019-03-27 09:46:35 +0000

    FLO-3362: Add cz parameter joi validation.

--- server/routes/location.js
+++ server/routes/location.js
@@ -25,7 +25,8 @@ module.exports = {
   options: {
     validate: {
       query: {
-        q: joi.string().required()
+        q: joi.string().required(),
+        cz: joi.string()
       }
     }
   }
