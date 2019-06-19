e5ef678f251c31ea93e07c29e1ed4e9ad80f09e1
John Shields <John.Shields@environment-agency.gov.uk>
2019-06-19 16:11:55 +0100

    FIS-131: Refactor active impacts code.

--- server/models/views/location.js
+++ server/models/views/location.js
@@ -2,14 +2,13 @@ const severity = require('../severity')
 const { groupBy } = require('../../util')
 
 class ViewModel {
-  constructor ({ place, floods, stations, impacts, activeImpacts }) {
+  constructor ({ place, floods, stations, impacts }) {
     const title = place.name
 
     Object.assign(this, {
       place,
       floods,
       impacts,
-      activeImpacts,
       location: title,
       pageTitle: `${title} flood risk`
     })
@@ -87,8 +86,8 @@ class ViewModel {
       this.groupedFloods = groupedFloods
       this.floodsPrimary = primaryStatement
       this.floodsSecondary = secondaryStatement + inactiveStatement
-      this.hasFloodsSecondary = this.floodsSecondary.length ? true : false
-      this.hasFloodsList = floods.length > primaryGroup.length || floods.length > 2 ? true : false
+      this.hasFloodsSecondary = !!this.floodsSecondary.length
+      this.hasFloodsList = !!(floods.length > primaryGroup.length || floods.length > 2)
       this.activeFloods = activeFloods
       this.hasActiveFloods = hasActiveFloods
       this.inactiveFloods = inactiveFloods
@@ -102,18 +101,13 @@ class ViewModel {
 
     // Impacts
     if (impacts) {
+      // sort impacts order by value
+      impacts.sort((a, b) => b.value - a.value)
+
+      // create an array of all active impacts
+      const activeImpacts = impacts.filter(active => active.telemetryactive === true)
+
       this.impacts = impacts
-    }
-
-    // sort impacts order by value
-    impacts.sort((a, b) => b.value - a.value)
-
-    // create an array of all active impacts
-    activeImpacts = impacts.filter(function (active) {
-      return active.telemetryactive === true
-    })
-
-    if (activeImpacts) {
       this.activeImpacts = activeImpacts
     }
   }
