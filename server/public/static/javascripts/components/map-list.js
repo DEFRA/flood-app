c3cbacd9ffbc619fc64209601cba14b27f9efcd4
danleech-defra <danleech.defra@gmail.com>
2019-06-20 17:30:02 +0100

    Show map layers and set key inputs

--- server/public/static/javascripts/components/map-list.js
+++ server/public/static/javascripts/components/map-list.js
@@ -1,33 +1,22 @@
 (function (window, flood) {
-  function MapList (settings, map) {
-    var containerEl = document.getElementById(settings.containerId)
+  function MapList (containerEl, settings) {
     var detailEl = containerEl.firstElementChild
 
     // Add map view button
-    var mapButton = document.createElement('button')
-    mapButton.innerText = settings.mapButtonText
-    mapButton.className = 'defra-button-map'
-    mapButton.addEventListener('click', function (e) {
-      e.preventDefault()
-      // Set activate layer(s)
-      var layers = document.querySelectorAll('.govuk-checkboxes__input')
-      layers.forEach(input => {
-        // input.checked = input.id === 'stations' ? 'checked' : ''
-      })
-      // Show map
-      map.container.show()
-    })
-    containerEl.prepend(mapButton)
+    this.mapButton = document.createElement('button')
+    this.mapButton.innerText = settings.mapButtonText
+    this.mapButton.className = 'defra-button-map'
+    containerEl.prepend(this.mapButton)
 
     // Optionally add list view button
     if (detailEl) {
       detailEl.setAttribute('hidden', 'hidden')
-      var listButton = document.createElement('button')
-      listButton.innerText = settings.listButtonText
-      listButton.className = 'defra-button-list'
-      listButton.setAttribute('aria-controls', detailEl.id)
-      listButton.setAttribute('aria-expanded', false)
-      listButton.addEventListener('click', function (e) {
+      this.listButton = document.createElement('button')
+      this.listButton.innerText = settings.listButtonText
+      this.listButton.className = 'defra-button-list'
+      this.listButton.setAttribute('aria-controls', detailEl.id)
+      this.listButton.setAttribute('aria-expanded', false)
+      this.listButton.addEventListener('click', function (e) {
         if (this.getAttribute('aria-expanded') === 'true') {
           this.setAttribute('aria-expanded', false)
           detailEl.setAttribute('hidden', 'hidden')
@@ -36,10 +25,10 @@
           detailEl.removeAttribute('hidden')
         }
       })
-      containerEl.prepend(listButton)
+      containerEl.prepend(this.listButton)
     }
   }
-  flood.createMapList = function (settings, map) {
-    return new MapList(settings, map)
+  flood.createMapList = function (containerEl, settings) {
+    return new MapList(containerEl, settings)
   }
 })(window, window.flood)
