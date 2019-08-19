aed8e62f275738f0d06381082925d4a666c545ce
Niki <niki.wycherley@environment-agency.gov.uk>
2019-08-19 14:41:57 +0100

    added basic route unit tests

--- test/routes/home.js
+++ test/routes/home.js
@@ -63,7 +63,117 @@ lab.experiment('Routes test - home', () => {
     sandbox.restore()
   })
 
-  lab.test('1 - GET /', async () => {
+  lab.test('1 - GET /consent', async () => {
+    const options = {
+      method: 'GET',
+      url: '/consent'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('2 - GET /start-page', async () => {
+    const options = {
+      method: 'GET',
+      url: '/start-page'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('3 - GET /location-not-england', async () => {
+    const options = {
+      method: 'GET',
+      url: '/location-not-england'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('4 - GET /sms-auto-opt-in-info', async () => {
+    const options = {
+      method: 'GET',
+      url: '/sms-auto-opt-in-info'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('5 - GET /what-to-do-in-a-flood/what-the-flood-warnings-mean', async () => {
+    const options = {
+      method: 'GET',
+      url: '/what-to-do-in-a-flood/what-the-flood-warnings-mean'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('6 - GET /plan-ahead-for-flooding', async () => {
+    const options = {
+      method: 'GET',
+      url: '/plan-ahead-for-flooding'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('7 - GET /what-happens-after-a-flood', async () => {
+    const options = {
+      method: 'GET',
+      url: '/what-happens-after-a-flood'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('8 - GET /recovering-after-a-flood', async () => {
+    const options = {
+      method: 'GET',
+      url: '/recovering-after-a-flood'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('9 - GET /what-to-do-in-a-flood/getting-a-flood-alert', async () => {
+    const options = {
+      method: 'GET',
+      url: '/what-to-do-in-a-flood/getting-a-flood-alert'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('10 - GET /what-to-do-in-a-flood/getting-a-flood-warning', async () => {
+    const options = {
+      method: 'GET',
+      url: '/what-to-do-in-a-flood/getting-a-flood-warning'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('11 - GET /what-to-do-in-a-flood/getting-a-severe-flood-warning', async () => {
+    const options = {
+      method: 'GET',
+      url: '/what-to-do-in-a-flood/getting-a-severe-flood-warning'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+  lab.test('12 - GET /', async () => {
     const options = {
       method: 'GET',
       url: '/'
