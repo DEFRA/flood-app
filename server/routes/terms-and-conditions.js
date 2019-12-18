45fb9323292c14d2b43a22106dacb3d0d0bf05fd
Niki <niki.wycherley@environment-agency.gov.uk>
2019-12-18 16:21:28 +0000

    added terms and conditions page and unit test

---
+++ server/routes/terms-and-conditions.js
@@ -0,0 +1,20 @@
+const description = 'This page explains the terms and conditions for using this site, our linking policy and the disclaimers attached to the information.'
+
+module.exports = {
+  method: 'GET',
+  path: '/terms-and-conditions',
+  options: {
+    description: 'Terms and conditions - Flood information service - GOV.UK',
+    handler: {
+      view: {
+        template: 'terms-and-conditions',
+        context: {
+          pageTitle: 'Terms and conditions - Flood information service - GOV.UK',
+          metaDescription: description,
+          ogDescription: description,
+          feedback: false
+        }
+      }
+    }
+  }
+}
