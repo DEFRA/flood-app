01e416e2aaec5d583937d32c411445662b3d15ab
John Shields <John.Shields@environment-agency.gov.uk>
2020-05-12 17:44:36 +0100

    FIS-526: Rework H1 and title components setting.

--- server/models/views/target-area.js
+++ server/models/views/target-area.js
@@ -6,7 +6,23 @@ class ViewModel {
     const severityLevel = flood && severity.filter(item => {
       return item.id === flood.severity_value
     })[0]
-    const pageTitle = severityLevel ? severityLevel.title + ' for ' + area.name : 'Flood risk target area: ' + area.name
+
+    const type = area.code.charAt(4).toLowerCase() === 'w'
+      ? 'warning'
+      : 'alert'
+
+    let pageTitle
+
+    if (severityLevel) {
+      if (severityLevel.isActive) {
+        pageTitle = severityLevel.title + ' for ' + area.name
+      }
+      if (!severityLevel.isActive || severityLevel.hash === 'removed') {
+        pageTitle = `${area.name} flood ${type} area`
+      }
+    } else {
+      pageTitle = `${area.name} flood ${type} area`
+    }
     const metaDescription = `Advice and guidance for residents living in the flood risk area: ${area.description}.`
     const metaCanonical = `/target-area/${area.code}`
 
