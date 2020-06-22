75bf654e551d74fa7cfde240360ee214c63344d0
danleech-defra <danleech-defra@gmail.com>
2020-06-22 09:35:34 +0100

    Console log removed

--- server/models/outlook.js
+++ server/models/outlook.js
@@ -93,8 +93,6 @@ class Outlook {
       })
     })
 
-    console.log(this._geoJson)
-
     // Smooth outlook polygons
     this._geoJson.features.forEach((feature) => {
       // Turf library used to create extra coordinates for Polygons
