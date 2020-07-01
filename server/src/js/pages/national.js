8dd272d91fab4ec1b6265b4f025f62bd66b3fdc1
John Shields <John.Shields@environment-agency.gov.uk>
2020-07-01 12:22:52 +0100

    FIS-592: Station page banner bug fix plus styling fixes.

--- server/src/js/pages/national.js
+++ server/src/js/pages/national.js
@@ -4,13 +4,23 @@ import '../components/map/maps'
 import '../components/map/styles'
 import '../components/map/layers'
 import '../components/map/container'
+import '../components/map/live'
 import '../components/map/outlook'
 
-// Create LiveMap
+// Create Live Map
+if (document.getElementById('map-live')) {
+  window.flood.maps.createLiveMap('map-live', {
+    btnText: 'View map of flood warnings and alerts',
+    btnClasses: 'defra-button-map',
+    layers: 'mv,ts,tw,ta'
+  })
+}
+
+// Create Outlook Map
 if (document.getElementById('map-outlook')) {
   window.flood.maps.createOutlookMap('map-outlook', {
     btnText: 'View map showing areas of concern',
-    btnClasses: 'defra-button-map govuk-!-margin-bottom-4',
+    btnClasses: 'defra-button-map',
     days: window.flood.model.outlook.days
   })
 }
