564a661f7695229b6c3a24c5c05285f9db14d2eb
tedd mason <teddmason@gmail.com>
2020-09-07 14:57:53 +0100

    some out of development updates for build jobs

--- server/routes/public.js
+++ server/routes/public.js
@@ -8,14 +8,6 @@ module.exports = [{
   }
 }, {
   method: 'GET',
-  path: '/assets/all.js',
-  options: {
-    handler: {
-      file: 'node_modules/govuk-frontend/govuk/all.js'
-    }
-  }
-}, {
-  method: 'GET',
   path: '/assets/{path*}',
   options: {
     handler: {
