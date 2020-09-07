e5e7259f33982818d2b57d93385c690641713706
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2020-09-07 11:26:23 +0100

    Feature/fis 560 (#112)
    * content changes for FIS-560
    
    * added hasWarningsRemoved for line above date

--- server/models/views/national.js
+++ server/models/views/national.js
@@ -10,7 +10,9 @@ class ViewModel {
       hasActiveFloods: floods.hasActiveFloods,
       highestSeverityId: floods.highestSeverityId,
       dateFormatted: 'Up to date as of ' + moment.tz('Europe/London').format('h:mma') + ' on ' + moment.tz('Europe/London').format('D MMMM YYYY'),
-      feedback: true
+      feedback: true,
+      hasWarningsRemoved: floods._groups[3].name === 4 && floods._groups[3].count > 0,
+      timestampOutlook: 'Updated at ' + moment(outlook._timestampOutlook).tz('Europe/London').format('h:mma') + ' on ' + moment(outlook._timestampOutlook).tz('Europe/London').format('D MMMM YYYY')
     })
 
     // Strip out flood array as it is superflous to the view
@@ -26,7 +28,7 @@ class ViewModel {
     // Strip out superflous outlook data
     this.outlook = [outlook].map(item => {
       return {
-        timestampOutlook: item.timestampOutlook,
+        timestampOutlook: this.timestampOutlook,
         full: item.full,
         hasOutlookConcern: item.hasOutlookConcern,
         days: item.days
