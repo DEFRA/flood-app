323b457238d831930d5b6fd32075d304a9690e59
sameer4414 <sameer4414@gmail.com>
2020-09-15 01:17:26 -0700

    FIS-678 : Date format change
    FIS-678 : Date format change

--- server/models/views/target-area.js
+++ server/models/views/target-area.js
@@ -37,9 +37,12 @@ class ViewModel {
     const areaDescription = `Flood ${type} area: ${area.description}`
     const parentAreaAlert = (!!(((flood && severityLevel.id === 4) && (type === 'warning')) || !flood) && (parentSeverityLevel && parentSeverityLevel.isActive))
 
-    const situationChanged = flood
+    let situationChanged = flood
       ? `Updated ${timeSituationChanged} on ${dateSituationChanged}`
       : `Up to date as of ${timeSituationChanged} on ${dateSituationChanged}`
+    if (flood && severityLevel.id === 4) {
+      situationChanged = `Removed ${timeSituationChanged} on ${dateSituationChanged}`
+    }
 
     const pageTitle = (severityLevel && severityLevel.isActive ? severityLevel.title + ' for ' + area.name : `${area.name} flood ${type} area`)
     const metaDescription = `Advice and guidance for residents living in the flood risk area: ${area.description}.`
