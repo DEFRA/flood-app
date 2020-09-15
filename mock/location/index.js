6fc3774feaaad92d7c166e2f3a2f10ad145c7405
John Shields <John.Shields@environment-agency.gov.uk>
2020-09-15 10:29:21 +0100

    FIS-696: Remove redundant mocking code.

--- mock/location/index.js
+++ mock/location/index.js
@@ -1,12 +1,8 @@
 const locationService = require('../../server/services/location')
 const find = require('./find.json')
-const geocode = require('./geocode.json')
-const reverseGeocode = require('./reverse-geocode.json')
 const mock = require('../../test/mock')
 
 /**
  * Override the real functions with mock implementations
  */
 locationService.find = mock.makePromise(null, find)
-locationService.geocode = mock.makePromise(null, geocode)
-locationService.reverseGeocode = mock.makePromise(null, reverseGeocode)
