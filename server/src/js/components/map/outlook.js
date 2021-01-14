bc5a5373a2918ef2676ee5309669e047dbcce62e
danleech-defra <danleech-defra@gmail.com>
2021-01-14 14:03:42 +0000

    Redundant month const removed

--- server/src/js/components/map/outlook.js
+++ server/src/js/components/map/outlook.js
@@ -186,14 +186,11 @@ function OutlookMap (mapId, options) {
   }
 
   const formatDate = (date) => {
-    // date.setDate(date.getDate() + 256)
     const number = date.getDate()
     const nth = (number) => {
       if (number > 3 && number < 21) return 'th'
       switch (number % 10) { case 1: return 'st'; case 2: return 'nd'; case 3: return 'rd'; default: return 'th' }
     }
-    const month = date.toLocaleString('en-GB', { month: 'short' })
-    // return month + ' ' + number + nth(number)
     return number + nth(number)
   }
 
