9a8a1d97ff5c6ec65a17de0853eec0c3bf975662
tedd mason <teddmason@gmail.com>
2021-04-12 12:37:58 +0100

    wip/stash

--- server/routes/river-and-sea-levels.js
+++ server/routes/river-and-sea-levels.js
@@ -1,9 +1,22 @@
 const joi = require('@hapi/joi')
+const boom = require('@hapi/boom')
 const ViewModel = require('../models/views/river-and-sea-levels')
 const floodService = require('../services/flood')
 const locationService = require('../services/location')
 const util = require('../util')
 const LocationNotFoundError = require('../location-not-found-error')
+
+const joiParams = joi.object({
+  q: joi.string().allow('').trim().max(200),
+  'river-id': joi.string(),
+  'target-area': joi.string(),
+  type: joi.string().allow('ri', 'c', 'g', 'ra'),
+  btn: joi.string(),
+  ext: joi.string(),
+  fid: joi.string(),
+  lyr: joi.string(),
+  v: joi.string()
+})
 
 module.exports = [{
   method: 'GET',
@@ -61,16 +74,7 @@ module.exports = [{
   },
   options: {
     validate: {
-      query: joi.object({
-        q: joi.string().allow('').trim().max(200),
-        'river-id': joi.string(),
-        'target-area': joi.string(),
-        btn: joi.string(),
-        ext: joi.string(),
-        fid: joi.string(),
-        lyr: joi.string(),
-        v: joi.string()
-      }),
+      query: joiParams,
       failAction: (request, h, err) => {
         return h.view('404').code(404).takeover()
       }
@@ -93,6 +97,21 @@ module.exports = [{
       }),
       failAction: (request, h, err) => {
         return h.view('river-and-sea-levels').takeover()
+      }
+    }
+  }
+}, {
+  method: 'POST',
+  path: '/river-and-sea-levels.json',
+  handler: async (request, h) => {
+    return ''
+  },
+  options: {
+    validate: {
+      payload: joiParams,
+      failAction: (request, h, err) => {
+        // TODO: is this the correct boom func
+        return boom.badImplementation(err)
       }
     }
   }
