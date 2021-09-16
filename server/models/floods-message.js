12185024d9205c4276421b4827841dd325645429
tedd mason <teddmason@gmail.com>
2021-09-16 15:34:09 +0100

    sonarcloud

--- server/models/floods-message.js
+++ server/models/floods-message.js
@@ -4,32 +4,28 @@ module.exports = (floods, isLocalised) => {
   const alerts = floods.groups[2].count
   const all = severeWarnings + warnings + alerts
   const text = getText(all, isLocalised)
-  const counts = getCounts(all, severeWarnings, warnings, alerts)
+  const counts = all === 0 ? 'no flood warnings or alerts' : getCounts(severeWarnings, warnings, alerts)
   return text.replace('$counts', counts)
 }
 
 const getText = (all, isLocalised) => `There ${all === 1 ? 'is' : 'are'} currently $counts in force${isLocalised ? ' at this location' : ''}.`
 
-const getCounts = (all, severeWarnings, warnings, alerts) => {
+const getCounts = (severeWarnings, warnings, alerts) => {
   let counts = ''
-  if (all === 0) {
-    counts = 'no flood warnings or alerts'
-  } else {
-    if (severeWarnings > 0) {
-      counts += getSevereWarnings(severeWarnings)
+  if (severeWarnings > 0) {
+    counts += getSevereWarnings(severeWarnings)
+  }
+  if (warnings > 0) {
+    if (counts.length > 0) {
+      counts += `${alerts > 0 ? ', ' : ' and '}`
     }
-    if (warnings > 0) {
-      if (counts.length > 0) {
-        counts += `${alerts > 0 ? ', ' : ' and '}`
-      }
-      counts += getWarnings(warnings)
+    counts += getWarnings(warnings)
+  }
+  if (alerts > 0) {
+    if (counts.length > 0) {
+      counts += ' and '
     }
-    if (alerts > 0) {
-      if (counts.length > 0) {
-        counts += ' and '
-      }
-      counts += getAlerts(alerts)
-    }
+    counts += getAlerts(alerts)
   }
   return counts
 }
