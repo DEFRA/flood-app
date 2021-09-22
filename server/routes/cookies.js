b4011070802b1c45a27d0870ee1d4cba55ac9c79
John Shields <John.Shields@environment-agency.gov.uk>
2021-09-22 16:10:13 +0100

    FSR-449: Ensure referer is from valid domain.

--- server/routes/cookies.js
+++ server/routes/cookies.js
@@ -1,3 +1,4 @@
+const { siteUrl } = require('../config')
 const description = 'The Environment Agency uses cookies to collect data about how users browse the site. This page explains what they do and how long they stay on your device.'
 
 module.exports = {
@@ -5,12 +6,13 @@ module.exports = {
   path: '/cookies',
   handler: async (request, h) => {
     const analyticsCookiesSet = Object.keys(request.state).some(key => /^_ga$|^_gid$|^_gat_gtag_./g.test(key))
+    const requestHeadersReferer = request.headers.referer && request.headers.referer.startsWith(siteUrl) ? request.headers.referer : ''
     return h.view('cookies', {
       pageTitle: 'Cookies - Flood information service - GOV.UK',
       heading: 'Flood information service',
       metaDescription: description,
       analyticsCookiesSet: analyticsCookiesSet,
-      referer: request.headers.referer || ''
+      referer: requestHeadersReferer
     })
   }
 }
