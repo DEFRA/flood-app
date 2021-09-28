bbe60f8eb03ac0047491a561bc8e054d6349f5f2
John Shields <shields.jf@gmail.com>
2021-09-28 10:06:58 +0100

    Service refresh - XSS vulnerability - feature/FSR-447 (#253)
    * FSR-449: Ensure referer is from valid domain.
    
    * FSR-447: Add unit test for invalid referer on cookies page.
    
    * FSR-447: Additional code to deal with xss attempt in referer.
    
    * removed a console log
    
    Co-authored-by: John Shields <John.Shields@environment-agency.gov.uk>
    Co-authored-by: NikiWycherley <niki.wycherley@environment-agency.gov.uk>

--- server/routes/cookies.js
+++ server/routes/cookies.js
@@ -1,3 +1,4 @@
+const { siteUrl } = require('../config')
 const description = 'The Environment Agency uses cookies to collect data about how users browse the site. This page explains what they do and how long they stay on your device.'
 
 module.exports = {
@@ -5,12 +6,22 @@ module.exports = {
   path: '/cookies',
   handler: async (request, h) => {
     const analyticsCookiesSet = Object.keys(request.state).some(key => /^_ga$|^_gid$|^_gat_gtag_./g.test(key))
+    let requestHeadersReferer = request.headers.referer && request.headers.referer.startsWith(siteUrl) ? encodeURI(request.headers.referer) : ''
+
+    if (requestHeadersReferer) {
+      // Attempt to ensure valid path structure
+      const urlStringArr = requestHeadersReferer.split(siteUrl)
+      if (urlStringArr[1] && !urlStringArr[1].startsWith('/') && !siteUrl.endsWith('/')) {
+        requestHeadersReferer = ''
+      }
+    }
+
     return h.view('cookies', {
       pageTitle: 'Cookies - Flood information service - GOV.UK',
       heading: 'Flood information service',
       metaDescription: description,
       analyticsCookiesSet: analyticsCookiesSet,
-      referer: request.headers.referer || ''
+      referer: requestHeadersReferer
     })
   }
 }
