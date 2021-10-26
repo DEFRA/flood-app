58ef6f67a4096d129eb9928ee6f437c3c70204a9
NikiWycherley <niki.wycherley@environment-agency.gov.uk>
2021-10-26 13:56:26 +0100

    just testing different ways untill AC is in

--- server/src/js/components/map/live.js
+++ server/src/js/components/map/live.js
@@ -423,18 +423,24 @@ function LiveMap (mapId, options) {
       model.state = feature.get('state')
       model.name = capitalise(model.station_name)
     }
+    console.log(model.one_hr_total)
+    console.log(model.six_hr_total)
+    console.log(model.day_total)
 
     // format rainfall
-    if (model.one_hr_total){
-      model.one_hr_total = Math.round(model.one_hr_total * 10) / 10
+    if (model.one_hr_total) {
+      model.one_hr_total = (Math.round(model.one_hr_total * 10) / 10).toFixed(1)
+      // model.one_hr_total = model.one_hr_total.toFixed(1)
     }
 
-    if (model.six_hr_total){
-      model.six_hr_total = Math.round(model.six_hr_total * 10) / 10
+    if (model.six_hr_total) {
+      model.six_hr_total = (Math.round(model.six_hr_total * 10) / 10).toFixed(1)
+      // model.six_hr_total = model.six_hr_total.toFixed(1)
     }
 
-    if (model.day_total){
-      model.day_total = Math.round(model.day_total * 10) / 10
+    if (model.day_total) {
+      model.day_total = (Math.round(model.day_total * 10) / 10).toFixed(1)
+      // model.day_total = model.day_total.toFixed(1)
     }
 
     const html = window.nunjucks.render('info-live.html', { model: model })
