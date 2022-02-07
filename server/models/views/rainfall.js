0a31eecebcc2651d4e1dea2f38fdb3f6f921cc80
NikiWycherley <niki.wycherley@environment-agency.gov.uk>
2022-02-07 14:44:01 +0000

    update to model

--- server/models/views/rainfall.js
+++ server/models/views/rainfall.js
@@ -1,7 +1,6 @@
-// const moment = require('moment-timezone')
-// const formatDate = require('../../util').formatDate
-// const { bingKeyMaps, floodRiskUrl } = require('../../config')
-// const tz = 'Europe/London'
+const { date } = require('@hapi/joi')
+const moment = require('moment-timezone')
+const tz = 'Europe/London'
 
 class ViewModel {
   constructor (rainfallStation, rainfallStationTotal) {
@@ -11,10 +10,47 @@ class ViewModel {
         'View current flood warnings and alerts for England and the national flood forecast for the next 5 days. Also check river, sea, groundwater and rainfall levels.',
       metaCanonical: '/',
       stationName: rainfallStationTotal[0].station_name,
-      latest1hr: rainfallStationTotal[0].one_hr_total,
-      latest6hr: rainfallStationTotal[0].six_hr_total,
-      latest24hr: rainfallStationTotal[0].day_total
+      telemetry: rainfallStation || []
     })
+
+    if (this.telemetry.length) {
+      const now = moment().tz(tz).format()
+      const fiveDaysAgo = moment().subtract(5, 'days').format()
+      
+      const latestDateTime = this.telemetry[0].value_timestamp
+      const dataStartDateTime = fiveDaysAgo
+      const rangeStartDateTime = fiveDaysAgo
+      const dataEndDateTime = now
+      const rangeEndDateTime = now
+      const latest1hr = rainfallStationTotal[0].one_hr_total
+      const latest6hr = rainfallStationTotal[0].six_hr_total
+      const latest24hr = rainfallStationTotal[0].day_total
+      this.period = this.telemetry[0].period
+
+      const values = this.telemetry.map(data => {
+        return { 
+          dateTime: data.value_timestamp,
+          value: data.value }
+      })
+      
+      values.sort((a, b) => b.dateTime - a.dateTime)
+
+      this.telemetryRainfall = {
+        latestDateTime,
+        dataStartDateTime,
+        rangeStartDateTime,
+        dataEndDateTime,
+        rangeEndDateTime,
+        latest1hr,
+        latest6hr,
+        latest24hr,
+        minutes: {
+          latestDateTime,
+          values
+        }
+      }
+
+    }
   }
 }
 
