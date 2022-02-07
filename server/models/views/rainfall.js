fff2146e55663f8717cdbf56282ed8cccb094682
LeeGordon83 <lee.gordon83@gmail.com>
2022-02-07 14:30:50 +0000

    rainfall model added

--- server/models/views/rainfall.js
+++ server/models/views/rainfall.js
@@ -4,13 +4,16 @@
 // const tz = 'Europe/London'
 
 class ViewModel {
-  constructor (rainfallStation) {
+  constructor (rainfallStation, rainfallStationTotal) {
     Object.assign(this, {
       pageTitle: 'Check for flooding in England',
       metaDescription:
         'View current flood warnings and alerts for England and the national flood forecast for the next 5 days. Also check river, sea, groundwater and rainfall levels.',
       metaCanonical: '/',
-      stationName: rainfallStation[0].station_name
+      stationName: rainfallStationTotal[0].station_name,
+      latest1hr: rainfallStationTotal[0].one_hr_total,
+      latest6hr: rainfallStationTotal[0].six_hr_total,
+      latest24hr: rainfallStationTotal[0].day_total
     })
   }
 }
