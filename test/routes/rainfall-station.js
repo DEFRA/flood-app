527b484fd334a77bf737c3f13d08bc2bd19e938d
NikiWycherley <niki.wycherley@environment-agency.gov.uk>
2022-02-10 11:32:13 +0000

    pushing up broken unit tests for other Dev to fix

--- test/routes/rainfall-station.js
+++ test/routes/rainfall-station.js
@@ -38,106 +38,6 @@ lab.experiment('Test - /rainfall-station', () => {
     await sandbox.restore()
   })
   lab.test('GET /rainfall-station', async () => {
-<<<<<<< HEAD
-    // const floodService = require('../../server/services/flood')
-
-
-
-    // const fakeStationsData = () => [
-    //   {
-    //     river_id: 'river-mersey',
-    //     river_name: 'River Mersey',
-    //     navigable: true,
-    //     view_rank: 3,
-    //     rank: 5,
-    //     rloi_id: 5149,
-    //     up: 5052,
-    //     down: 5050,
-    //     telemetry_id: '693976',
-    //     region: 'North West',
-    //     catchment: 'Lower Mersey',
-    //     wiski_river_name: 'River Mersey',
-    //     agency_name: 'Westy',
-    //     external_name: 'Westy',
-    //     station_type: 'S',
-    //     status: 'Active',
-    //     qualifier: 'u',
-    //     iswales: false,
-    //     value: '6.122',
-    //     value_timestamp: '2020-02-27T14:30:00.000Z',
-    //     value_erred: false,
-    //     percentile_5: '4.14',
-    //     percentile_95: '3.548',
-    //     centroid: '0101000020E6100000DF632687A47B04C09BC5867601B24A40',
-    //     lon: -2.56037240587146,
-    //     lat: 53.3906696470323
-    //   },
-    //   {
-    //     river_id: 'river-mersey',
-    //     river_name: 'River Mersey',
-    //     navigable: true,
-    //     view_rank: 3,
-    //     rank: 6,
-    //     rloi_id: 5050,
-    //     up: 5149,
-    //     down: 5084,
-    //     telemetry_id: '694063',
-    //     region: 'North West',
-    //     catchment: 'Lower Mersey',
-    //     wiski_river_name: 'River Mersey',
-    //     agency_name: 'Fiddlers Ferry',
-    //     external_name: 'Fiddlers Ferry',
-    //     station_type: 'S',
-    //     status: 'Active',
-    //     qualifier: 'u',
-    //     iswales: false,
-    //     value: '3.458',
-    //     value_timestamp: '2020-02-27T04:30:00.000Z',
-    //     value_erred: false,
-    //     percentile_5: '6.2',
-    //     percentile_95: '2.611',
-    //     centroid: '0101000020E61000001248AD04653A05C01F4188A7E5AF4A40',
-    //     lon: -2.65351298955739,
-    //     lat: 53.3741959967904
-    //   },
-    //   {
-    //     river_id: 'sankey-brook',
-    //     river_name: 'Sankey Brook',
-    //     navigable: true,
-    //     view_rank: 3,
-    //     rank: 1,
-    //     rloi_id: 5031,
-    //     up: null,
-    //     down: 5069,
-    //     telemetry_id: '694039',
-    //     region: 'North West',
-    //     catchment: 'Lower Mersey',
-    //     wiski_river_name: 'Sankey Brook',
-    //     agency_name: 'Causey Bridge',
-    //     external_name: 'Causey Bridge',
-    //     station_type: 'S',
-    //     status: 'Active',
-    //     qualifier: 'u',
-    //     iswales: false,
-    //     value: '0.111',
-    //     value_timestamp: '2020-02-27T14:30:00.000Z',
-    //     value_erred: false,
-    //     percentile_5: '2.5',
-    //     percentile_95: '0.209',
-    //     centroid: '0101000020E610000095683DBA03FA04C089E4A73671B64A40',
-    //     lon: -2.62207742214111,
-    //     lat: 53.4253300018109
-    //   }
-    // ]
-
-    // sandbox.stub(floodService, 'getIsEngland').callsFake(fakeIsEngland)
-    // sandbox.stub(floodService, 'getStationsWithin').callsFake(fakeStationsData)
-
-    // const fakeGetJson = () => data.warringtonGetJson
-
-    // const util = require('../../server/util')
-    // sandbox.stub(util, 'getJson').callsFake(fakeGetJson)
-=======
     const floodService = require('../../server/services/flood')
 
     const fakeRainfallStationsData = () => [
@@ -185,7 +85,6 @@ lab.experiment('Test - /rainfall-station', () => {
 
     sandbox.stub(floodService, 'getRainfallByStation').callsFake(fakeRainfallStationsData)
     sandbox.stub(floodService, 'getRainfallStationTotals').callsFake(fakeStationTotalsData)
->>>>>>> 8fc66b79f2b752b31756377273efc0f71d19644f
 
     const rainfallPlugin = {
       plugin: {
@@ -209,21 +108,6 @@ lab.experiment('Test - /rainfall-station', () => {
       url: '/rainfall-station/E24195'
     }
 
-<<<<<<< HEAD
-    console.log('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT')
-
-    const response = await server.inject(options)
-
-    // Code.expect(response.payload).to.contain('Low</span>')
-    // Code.expect(response.payload).to.contain('<li class="defra-flood-list-item defra-flood-list-item--S" data-river-id="sankey-brook" data-type="S">')
-    // Code.expect(response.payload).to.contain('Normal</span>')
-    // Code.expect(response.payload).to.contain('<li class="defra-flood-list-item defra-flood-list-item--S" data-river-id="river-mersey" data-type="S">')
-    // Code.expect(response.payload).to.contain('High</span>')
-    // Code.expect(response.payload).to.contain('<li class="defra-flood-list-item defra-flood-list-item--S" data-river-id="river-mersey" data-type="S">')
-    // Code.expect(response.payload).to.contain('3 levels')
-    // Code.expect(response.payload).to.contain('River Mersey')
-    // Code.expect(response.payload).to.contain('Sankey Brook')
-=======
     const response = await server.inject(options)
 
     console.log(response.payload)
@@ -232,7 +116,6 @@ lab.experiment('Test - /rainfall-station', () => {
     Code.expect(response.payload).to.contain('55.00mm')
     Code.expect(response.payload).to.contain('65.27mm')
     Code.expect(response.payload).to.contain('Lavenham')
->>>>>>> 8fc66b79f2b752b31756377273efc0f71d19644f
     Code.expect(response.statusCode).to.equal(200)
   })
 })
