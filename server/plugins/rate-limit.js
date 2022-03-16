f28e4ea8e240bf17bffab68e53f65cea337bc5ba
John Shields <John.Shields@environment-agency.gov.uk>
2022-03-16 16:06:59 +0000

    FSR-486: Revert to userLimit from userPathLimit.

--- server/plugins/rate-limit.js
+++ server/plugins/rate-limit.js
@@ -5,18 +5,18 @@ const rateLimit =
   plugin: require('hapi-rate-limit'),
   options: {
     enabled: false, // Set to 'false' if enabled on a per-route basis
-    userPathLimit: config.rateLimitRequests,
-    userPathCache: {
+    userLimit: config.rateLimitRequests,
+    userCache: {
       expiresIn: config.rateLimitExpiresIn * 1000 // Expiry time in seconds
     },
-    userLimit: false,
+    pathLimit: false,
     trustProxy: true,
     ipWhitelist: config.rateLimitWhitelist
   }
 }
 
 if (!config.localCache) {
-  rateLimit.options.userPathCache.cache = 'redis_cache'
+  rateLimit.options.userCache.cache = 'redis_cache'
 }
 
 module.exports = rateLimit
