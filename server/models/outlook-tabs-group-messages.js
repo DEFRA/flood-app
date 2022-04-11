7eb656e57c319b767254670b81580207a2243bdd
LeeGordon83 <lee.gordon83@gmail.com>
2022-04-11 15:41:45 +0100

    sonarcloud refactor

--- server/models/outlook-tabs-group-messages.js
+++ server/models/outlook-tabs-group-messages.js
@@ -47,9 +47,8 @@ class OutlookTabsGroupMessages {
     for (const [messageId, array] of Object.entries(groupByUniqueArrayObj)) {
       let sourcesArr = array.map(element => expandedSource[element.source] || element.source)
 
-      if (messageId === '3-i4-l2') {
-        sourcesArr[0] = sourcesArr[0].charAt(0).toUpperCase() + sourcesArr[0].slice(1)
-      }
+      capitaliseString(messageId, sourcesArr)
+
       if (sourcesArr.length > 1) {
         const lastSource = sourcesArr.pop()
         sourcesArr = `${sourcesArr.slice(0).join(', ')} and ${lastSource}`
@@ -62,6 +61,13 @@ class OutlookTabsGroupMessages {
 
     this.groupByDayMessage = groupByDayMessage
   }
+}
+
+function capitaliseString (messageId, sourcesArr) {
+  if (messageId === '3-i4-l2') {
+    sourcesArr[0] = sourcesArr[0].charAt(0).toUpperCase() + sourcesArr[0].slice(1)
+  }
+  return sourcesArr
 }
 
 module.exports = OutlookTabsGroupMessages
