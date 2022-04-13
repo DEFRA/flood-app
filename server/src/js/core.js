d837a42ace9f0929b4eae3a120e099d23076bb83
LeeGordon83 <lee.gordon83@gmail.com>
2022-04-13 15:14:34 +0100

    cookie reject added

--- server/src/js/core.js
+++ server/src/js/core.js
@@ -123,8 +123,6 @@ Array.prototype.forEach.call(jsElements, function (element) {
 
 if (cookieButtons) {
   const settingsButton = document.getElementById('cookie-settings')
-
-  // Accept button
   const acceptButton = document.createElement('button')
   const rejectButton = document.createElement('button')
 
