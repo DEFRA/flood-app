2adac9493ded00a61b8e15d80b16689d3b4d4c64
LeeGordon83 <lee.gordon83@gmail.com>
2023-02-07 09:02:59 +0000

    removed server log

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ createServer()
   .then((server) => {
     server.start()
     console.log('flood-app (%s) running on %s', pkg.version, server.info.uri)
-    if (config.fakeBingCall) {
-      server.log('info', 'Bing requests are being faked')
-    }
   })
   .catch(err => {
     console.log(err)
