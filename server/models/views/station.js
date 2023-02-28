3316a9a75d791e35187215bb0429a5930776b375
Cliff Pearson <cliff.pearson@capgemini.com>
2023-02-28 12:27:45 +0000

    Replaced use of hasOwnProperty

--- server/models/views/station.js
+++ server/models/views/station.js
@@ -298,7 +298,7 @@ class ViewModel {
 
     let imtdThreshold1 = imtdThresholds.thresholdsImtd[1]
 
-    if (!imtdThresholds.thresholdsImtd.hasOwnProperty('error') && imtdThreshold1 !== null) {
+    if (typeof imtdThresholds.thresholdsImtd.error !== 'undefined' && imtdThreshold1 !== null) {
       if (this.station.post_process) {
         if (stationStageDatum > 0) {
           imtdThreshold1 = imtdThreshold1 - stationStageDatum
@@ -318,7 +318,7 @@ class ViewModel {
 
     let imtdThreshold0 = imtdThresholds.thresholdsImtd[0]
 
-    if (!imtdThresholds.thresholdsImtd.hasOwnProperty('error') && imtdThreshold0 !== null) {
+    if (typeof imtdThresholds.thresholdsImtd.error !== 'undefined' && imtdThreshold0 !== null) {
       // Correct threshold value if value > zero (Above Ordnance Datum) [FSR-595]
       if (this.station.post_process) {
         if (stationStageDatum > 0) {
