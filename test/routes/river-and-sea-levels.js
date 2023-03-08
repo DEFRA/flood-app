5102adc60192bfed305aee51ffa7417408c9498b
LeeGordon83 <lee.gordon83@gmail.com>
2023-03-08 14:36:11 +0000

    added unit test coverage for trend

--- test/routes/river-and-sea-levels.js
+++ test/routes/river-and-sea-levels.js
@@ -478,6 +478,150 @@ lab.experiment('Test - /river-and-sea-levels', () => {
     Code.expect(response.payload).to.contain('<span class="defra-flood-levels-table-state defra-flood-levels-table-state--blue">HIGH</span>')
     Code.expect(response.statusCode).to.equal(200)
   })
+
+  lab.test('GET /river-and-sea-levels with trend falling, steady, rising ', async () => {
+    const floodService = require('../../server/services/flood')
+
+    const fakeIsEngland = () => {
+      return { is_england: true }
+    }
+
+    const fakeStationsData = () => [
+      {
+        river_id: 'river-mersey',
+        river_name: 'River Mersey',
+        navigable: true,
+        view_rank: 3,
+        rank: 5,
+        rloi_id: 5149,
+        up: 5052,
+        down: 5050,
+        telemetry_id: '693976',
+        region: 'North West',
+        catchment: 'Lower Mersey',
+        wiski_river_name: 'River Mersey',
+        agency_name: 'Westy',
+        external_name: 'Westy',
+        station_type: 'S',
+        status: 'Active',
+        qualifier: 'u',
+        iswales: false,
+        value: '6.122',
+        value_timestamp: '2020-02-27T14:30:00.000Z',
+        value_erred: false,
+        trend: 'steady',
+        percentile_5: '4.14',
+        percentile_95: '3.548',
+        centroid: '0101000020E6100000DF632687A47B04C09BC5867601B24A40',
+        lon: -2.56037240587146,
+        lat: 53.3906696470323
+      },
+      {
+        river_id: 'river-mersey',
+        river_name: 'River Mersey',
+        navigable: true,
+        view_rank: 3,
+        rank: 6,
+        rloi_id: 5050,
+        up: 5149,
+        down: 5084,
+        telemetry_id: '694063',
+        region: 'North West',
+        catchment: 'Lower Mersey',
+        wiski_river_name: 'River Mersey',
+        agency_name: 'Fiddlers Ferry',
+        external_name: 'Fiddlers Ferry',
+        station_type: 'S',
+        status: 'Active',
+        qualifier: 'u',
+        iswales: false,
+        value: '3.458',
+        value_timestamp: '2020-02-27T04:30:00.000Z',
+        value_erred: false,
+        trend: 'rising',
+        percentile_5: '6.2',
+        percentile_95: '2.611',
+        centroid: '0101000020E61000001248AD04653A05C01F4188A7E5AF4A40',
+        lon: -2.65351298955739,
+        lat: 53.3741959967904
+      },
+      {
+        river_id: 'sankey-brook',
+        river_name: 'Sankey Brook',
+        navigable: true,
+        view_rank: 3,
+        rank: 1,
+        rloi_id: 5031,
+        up: null,
+        down: 5069,
+        telemetry_id: '694039',
+        region: 'North West',
+        catchment: 'Lower Mersey',
+        wiski_river_name: 'Sankey Brook',
+        agency_name: 'Causey Bridge',
+        external_name: 'Causey Bridge',
+        station_type: 'S',
+        status: 'Active',
+        qualifier: 'u',
+        iswales: false,
+        value: '0.111',
+        value_timestamp: '2020-02-27T14:30:00.000Z',
+        value_erred: false,
+        trend: 'falling',
+        percentile_5: '2.5',
+        percentile_95: '0.209',
+        centroid: '0101000020E610000095683DBA03FA04C089E4A73671B64A40',
+        lon: -2.62207742214111,
+        lat: 53.4253300018109
+      }
+    ]
+
+    sandbox.stub(floodService, 'getIsEngland').callsFake(fakeIsEngland)
+    sandbox.stub(floodService, 'getStationsWithin').callsFake(fakeStationsData)
+
+    const fakeGetJson = () => data.warringtonGetJson
+
+    const util = require('../../server/util')
+    sandbox.stub(util, 'getJson').callsFake(fakeGetJson)
+
+    const riversPlugin = {
+      plugin: {
+        name: 'rivers',
+        register: (server, options) => {
+          server.route(require('../../server/routes/river-and-sea-levels'))
+        }
+      }
+    }
+
+    await server.register(require('../../server/plugins/views'))
+    await server.register(require('../../server/plugins/session'))
+    await server.register(riversPlugin)
+    // Add Cache methods to server
+    const registerServerMethods = require('../../server/services/server-methods')
+    registerServerMethods(server)
+
+    await server.initialize()
+    const options = {
+      method: 'GET',
+      url: '/river-and-sea-levels?q=Warrington'
+    }
+
+    const response = await server.inject(options)
+
+    const root = parse(response.payload)
+
+    checkTitleAndDescription(
+      root,
+      'Warrington - Find river, sea, groundwater and rainfall levels - GOV.UK',
+      'Find river, sea, groundwater and rainfall levels in Warrington. Check the last updated height and state recorded by the gauges.'
+    )
+
+    Code.expect(response.payload).to.contain('falling')
+    Code.expect(response.payload).to.contain('steady')
+    Code.expect(response.payload).to.contain('rising')
+    Code.expect(response.statusCode).to.equal(200)
+  })
+
   lab.test('GET /river-and-sea-levels station status Closed', async () => {
     const floodService = require('../../server/services/flood')
 
