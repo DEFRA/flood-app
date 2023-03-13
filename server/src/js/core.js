1494fa5a44a55c3a3a8e561b7e7db3e1e6ba5a53
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-03-13 17:10:15 +0000

    new way

--- server/src/js/core.js
+++ server/src/js/core.js
@@ -69,20 +69,21 @@ window.flood = {
       }
       const gtagManager = document.createElement('script')
 
-      gtagManager.onload =
-        (function (w, d, s, l, i) {
-          w[l] = w[l] || []
-          w[l].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-          })
-          var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : ''
-          j.async = true
-          j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-          f.parentNode.insertBefore(j, f)
-        })(window, document, 'script', 'dataLayer', 'G-TM9QYEJJPC')
+      const gtagFunc = function (w, d, s, l, i) {
+        w[l] = w[l] || []
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        })
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : ''
+        j.async = true
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+        f.parentNode.insertBefore(j, f)
+      }
+
+      gtagManager.onload = gtagFunc(window, document, 'script', 'dataLayer', 'G-TM9QYEJJPC')
 
       // <noscript><iframe src=`https://www.googletagmanager.com/ns.html?id=${process.env.GA_ID}` height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
