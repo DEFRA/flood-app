e058386a406775ceb1d4b4540906174302a16810
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-03-13 15:29:35 +0000

    reverting

--- server/src/js/core.js
+++ server/src/js/core.js
@@ -67,9 +67,17 @@ window.flood = {
         gtag('js', new Date())
         gtag('config', 'G-TM9QYEJJPC', { cookie_domain: document.domain })
       }
-      // const gtagManager = document.createElement('script')
+      const gtagManager = document.createElement('script')
 
-      const gtagTwo = document.head.innerHTML = "<script>(function (w, d, s, l, i) {w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js'}); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f)})(window, document, 'script', 'dataLayer', 'G-TM9QYEJJPC')</script>"
+      gtagManager.onload = () => {
+        (function (w, d, s, l, i) {
+          w[l] = w[l] || []; w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+          }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f)
+        })(window, document, 'script', 'dataLayer', 'G-TM9QYEJJPC')
+      }
 
       // <noscript><iframe src=`https://www.googletagmanager.com/ns.html?id=${process.env.GA_ID}` height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
@@ -81,8 +89,7 @@ window.flood = {
       iframe.setAttribute('style', 'display:none;visibility:hidden')
       noscript.appendChild(iframe)
 
-      // document.head.appendChild(gtagManager)
-      document.head.appendChild(gtagTwo)
+      document.head.appendChild(gtagManager)
       document.body.appendChild(script)
       document.body.appendChild(noscript)
     },
