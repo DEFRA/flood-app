6e148bf3bf01853ee3a983a81112a261111e7926
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-03-20 15:50:52 +0000

    linting

--- server/src/js/core.js
+++ server/src/js/core.js
@@ -71,7 +71,6 @@ window.flood = {
       const gtagManager = document.createElement('script')
       gtagManager.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer', ${process.env.GTM_ID});`
 
-
       const noscript = document.createElement('noscript')
       const iframe = document.createElement('iframe')
       iframe.setAttribute('src', `https://www.googletagmanager.com/ns.html?id=${process.env.GA4_ID}`)
