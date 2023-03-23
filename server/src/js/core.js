dcaad28fea0038c30bc960d35d084bac23f1853c
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-03-23 15:48:10 +0000

    quote marks around id in template

--- server/src/js/core.js
+++ server/src/js/core.js
@@ -69,7 +69,7 @@ window.flood = {
       }
 
       const gtagManager = document.createElement('script')
-      gtagManager.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer', ${process.env.GTM_ID});`
+      gtagManager.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_ID}');`
 
       const noscript = document.createElement('noscript')
       const iframe = document.createElement('iframe')
