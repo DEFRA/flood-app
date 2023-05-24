157528ae61590e583146af9571bc0f6e6ddd56ff
Chris Capper <christopher.capper@environment-agency.gov.uk>
2023-05-24 09:28:52 +0100

    FSR-864 Normal range label on station page using old logic (#443)
    * Branch for fixing normal range logic on station page https://eaflood.atlassian.net/browse/FSR-864
    
    * Removed logic changing range content when low or high - logic now state normal range between percentiles, and if no percentiles don't show.  Modified 1 unit test and added 1 new unit test

--- test/routes/station.js
+++ test/routes/station.js
@@ -406,7 +406,7 @@ lab.experiment('Test - /station/{id}', () => {
     Code.expect(response.payload).to.contain('River Ribble level at Walton-Le-Dale - GOV.UK')
     Code.expect(response.payload).to.contain('Normal')
     Code.expect(response.payload).to.contain('Steady')
-    Code.expect(response.payload).to.contain('0.15m to 3.50m')
+    Code.expect(response.payload).to.contain('Normal range 0.15m to 3.50m')
     Code.expect(response.payload).to.contain('<a data-journey-click="Station:Station navigation:Station - Nearby levels" href="/river-and-sea-levels/rloi/5146" class="defra-flood-nav__link">Nearby levels</a>')
     Code.expect(response.payload).to.contain('<a href="/station/5122" class="defra-flood-nav__link defra-flood-nav__link--upstream">Upstream</a>')
     Code.expect(response.payload).to.contain('<h2 class="govuk-heading-s govuk-!-margin-top-0">Share this page</h2>')
@@ -2235,5 +2235,142 @@ lab.experiment('Test - /station/{id}', () => {
     Code.expect(response.statusCode).to.equal(200)
     Code.expect(response.payload).to.not.contain('Low lying land flooding is possible above this level. One or more flood alerts may be issued')
     Code.expect(response.payload).to.not.contain('Property flooding is possible above this level. One or more flood warnings may be issued')
+  })
+  lab.test('GET station/5146 with missing percentile ', async () => {
+    const floodService = require('../../server/services/flood')
+
+    const fakeStationData = () => {
+      return {
+        rloi_id: 5146,
+        station_type: 'S',
+        qualifier: 'u',
+        telemetry_context_id: '1146588',
+        telemetry_id: '713030',
+        wiski_id: '713030',
+        post_process: false,
+        subtract: null,
+        region: 'North West',
+        area: 'Cumbria and Lancashire',
+        catchment: 'Ribble Douglas and Crossens',
+        display_region: 'North West',
+        display_area: '',
+        display_catchment: '',
+        agency_name: 'Walton-Le-Dale',
+        external_name: 'Walton-Le-Dale',
+        location_info: 'Preston',
+        x_coord_actual: 355230,
+        y_coord_actual: 428720,
+        actual_ngr: '',
+        x_coord_display: 355230,
+        y_coord_display: 428720,
+        site_max: '5',
+        wiski_river_name: 'River Ribble',
+        date_open: '2001-01-01T00:00:00.000Z',
+        stage_datum: '3.642',
+        period_of_record: 'to date',
+        por_max_value: '5.488',
+        date_por_max: '2020-02-09T18:15:00.000Z',
+        highest_level: '3.469',
+        date_highest_level: '2012-09-26T01:15:00.000Z',
+        por_min_value: '-0.07',
+        date_por_min: '2009-04-22T12:45:00.000Z',
+        percentile_5: '3.5',
+        comments: '',
+        status: 'Active',
+        status_reason: '',
+        status_date: null,
+        coordinates: '{"type":"Point","coordinates":[-2.68044442027032,53.7529105624953]}',
+        geography: '0101000020E61000001A741ED88C7105C0755D915F5FE04A40',
+        centroid: '0101000020E61000001A741ED88C7105C0755D915F5FE04A40'
+      }
+    }
+
+    const fakeRiverData = () => {
+      return {
+        river_id: 'river-ribble',
+        river_name: 'River Ribble',
+        navigable: true,
+        view_rank: 3,
+        rank: 9,
+        rloi_id: 5146,
+        up: 5122,
+        down: null,
+        telemetry_id: '713030',
+        region: 'North West',
+        catchment: 'Ribble Douglas and Crossens',
+        wiski_river_name: 'River Ribble',
+        agency_name: 'Walton-Le-Dale',
+        external_name: 'Walton-Le-Dale',
+        station_type: 'S',
+        status: 'Active',
+        qualifier: 'u',
+        iswales: false,
+        value: '9.567',
+        value_timestamp: '2020-03-17T04:30:00.000Z',
+        value_erred: false,
+        trend: 'steady',
+        percentile_5: '3.5',
+        percentile_95: '0.15',
+        centroid: '0101000020E61000001A741ED88C7105C0755D915F5FE04A40',
+        lon: -2.68044442027032,
+        lat: 53.7529105624953,
+        id: '2695'
+      }
+    }
+
+    const fakeTelemetryData = () => [
+      {
+        ts: '2022-02-08T08:30:00.000Z',
+        _: 1.354,
+        err: false
+      }
+    ]
+
+    const fakeImpactsData = () => []
+    const fakeThresholdsData = () => []
+    const fakeTargetAreasData = () => []
+    const fakeStationThresholdData = () => { return { alert: null, warning: null } }
+
+    sandbox.stub(floodService, 'getStationById').callsFake(fakeStationData)
+    sandbox.stub(floodService, 'getRiverStationByStationId').callsFake(fakeRiverData)
+    sandbox.stub(floodService, 'getStationTelemetry').callsFake(fakeTelemetryData)
+    sandbox.stub(floodService, 'getImpactData').callsFake(fakeImpactsData)
+    sandbox.stub(floodService, 'getStationForecastThresholds').callsFake(fakeThresholdsData)
+    sandbox.stub(floodService, 'getStationImtdThresholds').callsFake(fakeStationThresholdData)
+    sandbox.stub(floodService, 'getWarningsAlertsWithinStationBuffer').callsFake(fakeTargetAreasData)
+
+    const stationPlugin = {
+      plugin: {
+        name: 'station',
+        register: (server, options) => {
+          server.route(require('../../server/routes/station'))
+        }
+      }
+    }
+
+    await server.register(require('../../server/plugins/views'))
+    await server.register(require('../../server/plugins/session'))
+    await server.register(stationPlugin)
+    // Add Cache methods to server
+    const registerServerMethods = require('../../server/services/server-methods')
+    registerServerMethods(server)
+
+    await server.initialize()
+    const options = {
+      method: 'GET',
+      url: '/station/5146'
+    }
+
+    const response = await server.inject(options)
+
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.payload).to.contain('River Ribble level at Walton-Le-Dale - GOV.UK')
+    Code.expect(response.payload).to.contain('Normal')
+    Code.expect(response.payload).to.contain('Steady')
+    Code.expect(response.payload).to.not.contain('Normal range ')
+    Code.expect(response.payload).to.contain('<a data-journey-click="Station:Station navigation:Station - Nearby levels" href="/river-and-sea-levels/rloi/5146" class="defra-flood-nav__link">Nearby levels</a>')
+    Code.expect(response.payload).to.contain('<a href="/station/5122" class="defra-flood-nav__link defra-flood-nav__link--upstream">Upstream</a>')
+    Code.expect(response.payload).to.contain('<h2 class="govuk-heading-s govuk-!-margin-top-0">Share this page</h2>')
+    Code.expect(response.payload).to.contain('<a data-journey-click="Station:Station data:Station - Download csv" href="/station-csv/5146" class="defra-button-secondary defra-button-secondary--icon"><svg focusable="false" aria-hidden="true" width="14" height="20" viewBox="0 0 14 20"><path d="M1.929 9L7 14.071 12.071 9M7 14.071V1M1 18h12" fill="none" stroke="currentColor" stroke-width="2"/></svg>Download height data CSV (12KB)</a>')
   })
 })
