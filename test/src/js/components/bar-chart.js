fd460e59fa382a88656fc444eb6e0b044aff29b0
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-07-20 11:58:42 +0100

    Add pagination tests

--- test/src/js/components/bar-chart.js
+++ test/src/js/components/bar-chart.js
@@ -41,10 +41,6 @@ experiment('BarChart', () => {
     expect(controlsContainer).not.to.equal(null)
     expect(controls).to.have.length(2)
 
-    expect([...controls[0].classList]).to.equal([
-      'defra-chart-segmented-control__segment',
-      'defra-chart-segmented-control__segment--selected'
-    ])
     expect(controls[0].children[1].textContent).to.equal('Hours')
     expect({ ...controls[0].children[0].dataset }).to.equal({
       period: 'hours',
@@ -52,15 +48,15 @@ experiment('BarChart', () => {
       end: '2023-07-19T00:00:00Z'
     })
 
-    expect([...controls[1].classList]).to.equal([
-      'defra-chart-segmented-control__segment'
-    ])
     expect(controls[1].children[1].textContent).to.equal('Minutes')
     expect({ ...controls[1].children[0].dataset }).to.equal({
       period: 'minutes',
       start: '2023-07-18T00:00:00Z',
       end: '2023-07-19T00:00:00Z'
     })
+
+    const description = chartContainer.querySelector('#bar-chart-description').textContent
+    expect(description).to.startWith('\n    Showing 5 days\n    from 14 July 2023 at 2:00AM to 18 July 2023 at 4:00PM in hourly totals.')
   })
 
   test('The minutes control switches the chart to 24 hour range', async () => {
@@ -71,18 +67,73 @@ experiment('BarChart', () => {
     chartContainer.setAttribute('id', 'bar-chart-container')
     document.body.appendChild(chartContainer)
     window.flood.charts.createBarChart('bar-chart-container', chartId, telemetry)
-    const [hoursControl, minutesControl] = chartContainer.querySelector('.defra-chart-segmented-control').children
+    const minutesControl = chartContainer.querySelector('.defra-chart-segmented-control').children[1]
 
     // Act
     minutesControl.children[0].click()
 
     // Assert
-    expect([...minutesControl.classList]).to.equal([
-      'defra-chart-segmented-control__segment',
-      'defra-chart-segmented-control__segment--selected'
-    ])
-    expect([...hoursControl.classList]).to.equal([
-      'defra-chart-segmented-control__segment'
-    ])
+    const description = chartContainer.querySelector('#bar-chart-description').textContent
+    expect(description).to.startWith('\n    Showing 24 hours\n    from 18 July 2023 at 1:15AM to 18 July 2023 at 3:45PM in 15 minute totals.')
+  })
+
+  test('The pagination buttons are shown when the chart is in the 24 hour range', async () => {
+    // Arrange
+    const chartId = 'example-chart-id'
+    const telemetry = telemetryFixture
+    const chartContainer = document.createElement('div')
+    chartContainer.setAttribute('id', 'bar-chart-container')
+    document.body.appendChild(chartContainer)
+
+    // Act
+    window.flood.charts.createBarChart('bar-chart-container', chartId, telemetry)
+    chartContainer.querySelector('.defra-chart-segmented-control__segment input[data-period="minutes"]').click()
+
+    // Assert
+    const outerContainer = document.querySelector('.defra-chart-pagination')
+    const innerContainer = outerContainer.querySelector('.defra-chart-pagination_inner')
+    expect(outerContainer).not.to.equal(null)
+    expect(innerContainer.style.display).to.equal('inline-block')
+    expect(innerContainer.children).to.have.length(2)
+
+    expect(innerContainer.children[0].dataset.direction).to.equal('back')
+    expect(innerContainer.children[1].dataset.direction).to.equal('forward')
+  })
+
+  test('The pagination buttons are not shown when the chart is in the 5 day range', async () => {
+    // Arrange
+    const chartId = 'example-chart-id'
+    const telemetry = telemetryFixture
+    const chartContainer = document.createElement('div')
+    chartContainer.setAttribute('id', 'bar-chart-container')
+    document.body.appendChild(chartContainer)
+
+    // Act
+    window.flood.charts.createBarChart('bar-chart-container', chartId, telemetry)
+    chartContainer.querySelector('.defra-chart-segmented-control__segment input[data-period="hours"]').click()
+
+    // Assert
+    const outerContainer = document.querySelector('.defra-chart-pagination')
+    const innerContainer = outerContainer.querySelector('.defra-chart-pagination_inner')
+    expect(outerContainer).not.to.equal(null)
+    expect(innerContainer.style.display).to.equal('none')
+  })
+
+  test('The pagination buttons allow changing the page range', async () => {
+    // Arrange
+    const chartId = 'example-chart-id'
+    const telemetry = telemetryFixture
+    const chartContainer = document.createElement('div')
+    chartContainer.setAttribute('id', 'bar-chart-container')
+    document.body.appendChild(chartContainer)
+
+    // Act
+    window.flood.charts.createBarChart('bar-chart-container', chartId, telemetry)
+    chartContainer.querySelector('.defra-chart-segmented-control__segment input[data-period="minutes"]').click()
+    chartContainer.querySelector('.defra-chart-pagination__button--back').click()
+
+    // Assert
+    const description = chartContainer.querySelector('#bar-chart-description').textContent
+    expect(description).to.startWith('\n    Showing 24 hours\n    from 17 July 2023 at 1:15AM to 18 July 2023 at 1:15AM in 15 minute totals')
   })
 })
