7eaf8b124820639445de4756c222744d81df293a
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-08-11 10:37:17 +0100

    fix custom class support on map buttons

--- server/src/js/components/map/button.js
+++ server/src/js/components/map/button.js
@@ -11,7 +11,7 @@ export function createMapButton (buttonContainer, uri, options) {
   }
   button.id = mapId + '-btn'
   button.innerHTML = `<span class="defra-button-secondary__icon"><svg focusable="false" width="15" height="20" viewBox="0 0 15 20"><path d="M15,7.5c0.009,3.778 -4.229,9.665 -7.5,12.5c-3.271,-2.835 -7.509,-8.722 -7.5,-12.5c0,-4.142 3.358,-7.5 7.5,-7.5c4.142,0 7.5,3.358 7.5,7.5Zm-7.5,5.461c3.016,0 5.461,-2.445 5.461,-5.461c0,-3.016 -2.445,-5.461 -5.461,-5.461c-3.016,0 -5.461,2.445 -5.461,5.461c0,3.016 2.445,5.461 5.461,5.461Z" fill="currentColor"/></svg></span><span class="defra-button-secondary__text">${options.btnText || 'View map'}</span><span class="govuk-visually-hidden">(Visual only)</span>`
-  button.className = options.btnClass || (options.btnType === 'link' ? 'defra-link-icon-s' : 'defra-button-secondary')
+  button.className = options.btnClasses || (options.btnType === 'link' ? 'defra-link-icon-s' : 'defra-button-secondary')
 
   if (options.data && options.data.button) {
     button.setAttribute('data-journey-click', options.data.button)
