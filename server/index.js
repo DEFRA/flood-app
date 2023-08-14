90fe5bfea227812d77aaa3c9483f618af1314f42
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-08-14 15:27:43 +0100

    update @hapi/hapi

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ if (!config.localCache) {
   cache = [{
     name: 'redis_cache',
     provider: {
-      constructor: CatboxRedis,
+      constructor: CatboxRedis.Engine,
       options: {
         host: config.redisHost,
         port: config.redisPort,
