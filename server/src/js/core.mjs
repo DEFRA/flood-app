7cc8073243fb508392c0b066f4883b31070474e1
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-08-16 16:22:36 +0100

    FSR-954 - Implement Google Tags (#481)
    * impliment google tag manager
    
    https://eaflood.atlassian.net/browse/FSR-954
    
    * added code for GTM

--- server/src/js/core.mjs
+++ server/src/js/core.mjs
@@ -109,6 +109,22 @@ window.flood = {
         gtag('js', new Date())
         gtag('config', process.env.GA4_ID, { cookie_domain: window.location.hostname })
       }
+
+      const gtagManager = document.createElement('script')
+      gtagManager.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_ID}');`
+
+      const noscript = document.createElement('noscript')
+      const iframe = document.createElement('iframe')
+      iframe.setAttribute('src', `https://www.googletagmanager.com/ns.html?id=${process.env.GA4_ID}`)
+      iframe.setAttribute('height', '0')
+      iframe.setAttribute('width', '0')
+      iframe.setAttribute('style', 'display:none;visibility:hidden')
+      noscript.appendChild(iframe)
+
+      const head = document.getElementsByTagName('head')[0]
+      head.insertBefore(gtagManager, head.firstChild)
+      document.head.appendChild(gtagManager)
+      document.body.appendChild(noscript)
       document.body.appendChild(script)
     },
     setGoogleAnalyticsEvent: () => {
