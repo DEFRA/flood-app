816cd489a8258b35d11d473b3fc5ceaf5c7461a8
Ash Taylor <ashtaylor@ip-192-168-1-247.eu-west-2.compute.internal>
2023-10-03 10:09:59 +0100

    FSR-985 add webchat

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -1,4 +1,26 @@
+'use strict'
+
+const getAvailability = require('@defra/flood-webchat/dist/server')
+
+const webchat = [{
+  method: 'GET',
+  path: '/webchat-availability',
+  handler: async () => {
+    const response = await getAvailability({
+      clientId: process.env.CXONE_CLIENT_ID,
+      clientSecret: process.env.CXONE_CLIENT_SECRET,
+      accessKey: process.env.CXONE_ACCESS_KEY,
+      accessSecret: process.env.CXONE_ACCESS_SECRET,
+      skillEndpoint: process.env.CXONE_SKILL_ENDPOINT,
+      hoursEndpoint: process.env.CXONE_HOURS_ENDPOINT,
+      maxQueueCount: process.env.CXONE_MAX_QUEUE_COUNT
+    })
+    return response
+  }
+}]
+
 const routes = [].concat(
+  webchat,
   require('../routes/find-location'),
   require('../routes/location'),
   require('../routes/national'),
