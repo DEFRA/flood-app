82d21f5be7ebce9a90f8b81edf40e04f48c018bb
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-10-17 09:14:50 +0100

    disable requestTimeout and headerTimeout as we run behind a reverse proxy
    see:
    - https://nodejs.org/api/http.html#serverheaderstimeout
    - https://nodejs.org/api/http.html#serverrequesttimeout

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const pino = require('./server/lib/pino')
 
 createServer()
   .then((server) => {
+    server.listener.requestTimeout = 0
+    server.listener.headersTimeout = 0
     server.start()
   })
   .catch(err => {
