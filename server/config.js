22cc02e3b9a452291928554cf0da10877d580549
Max Bladen-Clark <max.bladen-clark@defra.gov.uk>
2023-11-13 15:20:47 +0000

    update to mock out webchat backend

--- server/config.js
+++ server/config.js
@@ -41,6 +41,18 @@ const schema = joi.object({
     host: joi.string().default('https://errbit-prd.aws-int.defra.cloud'),
     projectId: joi.number().default(1),
     projectKey: joi.string()
+  }),
+  webchat: joi.object({
+    enabled: joi.boolean().default(false),
+    clientId: joi.string(),
+    clientSecret: joi.string(),
+    accessKey: joi.string(),
+    accessSecret: joi.string(),
+    authenticationUri: joi.string(),
+    wellKnownUri: joi.string(),
+    skillEndpoint: joi.string(),
+    hoursEndpoint: joi.string(),
+    maxQueueCount: joi.string()
   })
 })
 
@@ -79,6 +91,18 @@ const config = {
     host: process.env.ERRBIT_HOST,
     projectId: process.env.ERRBIT_PROJECT_ID,
     projectKey: process.env.ERRBIT_PROJECT_KEY
+  },
+  webchat: {
+    enabled: process.env.WEBCHAT_ENABLED === 'true',
+    clientId: process.env.CXONE_CLIENT_ID,
+    clientSecret: process.env.CXONE_CLIENT_SECRET,
+    accessKey: process.env.CXONE_ACCESS_KEY,
+    accessSecret: process.env.CXONE_ACCESS_SECRET,
+    authenticationUri: process.env.CXONE_AUTHENTICATION_URI,
+    wellKnownUri: process.env.CXONE_WELL_KNOWN_URI,
+    skillEndpoint: process.env.CXONE_SKILL_ENDPOINT,
+    hoursEndpoint: process.env.CXONE_HOURS_ENDPOINT,
+    maxQueueCount: process.env.CXONE_MAX_QUEUE_COUNT
   }
 }
 
