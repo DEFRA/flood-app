42ae9fdab58c251def1e0e4ad64c387d96b063de
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2023-12-19 17:17:18 +0000

    First cut at validation of location search input (WIP)

--- server/routes/national.js
+++ server/routes/national.js
@@ -1,8 +1,9 @@
 const OutlookModel = require('../models/outlook')
 const FloodsModel = require('../models/floods')
 const ViewModel = require('../models/views/national')
+const joi = require('@hapi/joi')
 
-module.exports = {
+module.exports = [{
   method: 'GET',
   path: '/',
   handler: async (request, h) => {
@@ -23,4 +24,29 @@ module.exports = {
 
     return h.view('national', { model })
   }
-}
+}, {
+  method: 'POST',
+  path: '/',
+  handler: async (request, h) => {
+    const { location } = request.payload
+    if (location) {
+      return h.redirect(`/location?q=${encodeURIComponent(location)}`)
+    }
+    //  TODO: decide if we need to use redirect or view here. Would need to extract the model building in GET to a method
+    return h.redirect('/')
+  },
+  options: {
+    validate: {
+      payload: joi.object({
+        // TODO: check list of allowed characters
+        location: joi.string().allow('').regex(/^[ a-zA-Z0-9-!]+$/).min(2).required()
+      }),
+      failAction: (request, h, err) => {
+        request.logger.warn({ situation: 'location search failed validation', err })
+        // const model = new ViewModel({ err })
+        // model.referer = request.headers.referer
+        // return h.view('/', { model }).takeover()
+      }
+    }
+  }
+}]
