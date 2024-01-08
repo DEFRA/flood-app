55a7936d52372214af3e5a5e547f85df919967af
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2024-01-08 10:42:08 +0000

    Replace find-location route handler with redirect
    * This is in addition to the infrastructure redirect
    * Remove find-location model and view
    * Replace find-location links with root links
    * Display-error did not appear to be used so deleted
    * Updated tests accordingly

--- server/routes/find-location.js
+++ server/routes/find-location.js
@@ -1,35 +1,10 @@
 'use strict'
 
-const joi = require('@hapi/joi')
-const ViewModel = require('../models/views/find-location')
-
 module.exports = [{
   method: 'GET',
   path: '/find-location',
   handler: async (request, h) => {
-    const err = {}
-    const location = ''
-    const model = new ViewModel({ location, err })
-    model.referer = request.headers.referer
-    return h.view('find-location', { model })
-  }
-}, {
-  method: 'POST',
-  path: '/find-location',
-  handler: async (request, h) => {
-    const { location } = request.payload
-    return h.redirect(`/location?q=${encodeURIComponent(location)}`)
-  },
-  options: {
-    validate: {
-      payload: joi.object({
-        location: joi.string().required()
-      }),
-      failAction: (request, h, err) => {
-        const model = new ViewModel({ err })
-        model.referer = request.headers.referer
-        return h.view('find-location', { model }).takeover()
-      }
-    }
+    // note: this handler can be removed once the redirect is added at the infrastructure level
+    return h.redirect('/').code(301)
   }
 }]
