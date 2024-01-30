4ae664e80ae382958009c5cd598efd7e8bff2921
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2024-01-30 10:23:52 +0000

    FSR-1131 | Improve sonar cloud quality check (#609)
    * https://eaflood.atlassian.net/browse/FSR-1131
    
    * moved part of contructor out to reduce complexity
    
    * sonar
    
    * Changed concatonation to template literal
    
    * changed the for nesting to be more readable
    
    * fixing sonarcloud issues
    
    * changed method name
    
    * updated test to look better using destructuring assignment

--- server/models/outlook-polys.js
+++ server/models/outlook-polys.js
@@ -6,46 +6,53 @@ const RISK_LEVELS = new Map([
   [3, new Map([[1, 2], [2, 2], [3, 3], [4, 3]])],
   [4, new Map([[1, 2], [2, 3], [3, 3], [4, 4]])]
 ])
+
 module.exports = class OutlookPolys {
   constructor (outlook, place) {
     this.polys = []
 
-    const locationCoords = turf.polygon([[
-      [place.bbox2k[0], place.bbox2k[1]],
-      [place.bbox2k[0], place.bbox2k[3]],
-      [place.bbox2k[2], place.bbox2k[3]],
-      [place.bbox2k[2], place.bbox2k[1]],
-      [place.bbox2k[0], place.bbox2k[1]]
-    ]])
+    const locationCoords = turf.polygon([
+      [
+        [place.bbox2k[0], place.bbox2k[1]],
+        [place.bbox2k[0], place.bbox2k[3]],
+        [place.bbox2k[2], place.bbox2k[3]],
+        [place.bbox2k[2], place.bbox2k[1]],
+        [place.bbox2k[0], place.bbox2k[1]]
+      ]
+    ])
 
-    for (const riskArea of outlook.risk_areas) {
-      for (const riskAreaBlock of riskArea.risk_area_blocks) {
-        for (const poly of riskAreaBlock.polys) {
+    outlook.risk_areas.forEach(riskArea => {
+      riskArea.risk_area_blocks.forEach(riskAreaBlock => {
+        riskAreaBlock.polys.forEach(poly => {
           // build array of polys that intersect
           if (!turf.intersect(getPolyCoords(poly), locationCoords)) {
-            continue
+            return
           }
+          riskAreaBlock.days.forEach(day => {
+            this.buildRiskAreaArray(riskAreaBlock, day, poly)
+          })
+        })
+      })
+    })
 
-          for (const day of riskAreaBlock.days) {
-            for (const [key, [impact, likelihood]] of Object.entries(riskAreaBlock.risk_levels)) {
-              if (impact > 1 && !(impact === 2 && likelihood === 1)) {
-                const riskLevel = RISK_LEVELS.get(impact).get(likelihood)
-                this.polys.push({
-                  riskLevel,
-                  impact,
-                  likelihood,
-                  day,
-                  polyId: poly.id,
-                  source: key,
-                  messageId: `${riskLevel}-i${impact}-l${likelihood}`
-                })
-              }
-            }
-          }
-        }
+    this.polys.sort(sortPolys)
+  }
+
+  buildRiskAreaArray (riskAreaBlock, day, poly) {
+    for (const [key, [impact, likelihood]] of Object.entries(riskAreaBlock.risk_levels)) {
+      if (impact > 1 && !(impact === 2 && likelihood === 1)) {
+        const riskLevel = RISK_LEVELS.get(impact).get(likelihood)
+        this.polys.push({
+          riskLevel,
+          impact,
+          likelihood,
+          day,
+          polyId: poly.id,
+          source: key,
+          messageId: `${riskLevel}-i${impact}-l${likelihood}`
+        })
       }
     }
-    this.polys.sort(sortPolys)
   }
 }
 
