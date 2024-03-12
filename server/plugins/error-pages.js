f96082b42d9d74f4b234a9217f69f5a7a62ae84d
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2024-03-12 14:16:35 +0000

    changed numbers to words

--- server/plugins/error-pages.js
+++ server/plugins/error-pages.js
@@ -21,18 +21,21 @@ module.exports = {
         let logLevel
         let statusCode
 
+        const HTTP_BAD_REQUEST = 400
         const HTTP_NOT_FOUND = 404
+        const HTTP_TOO_MANY_REQUESTS = 429
+        const INTERNAL_SERVER_ERROR = 500
 
-        if (response.output.statusCode === 500 && response.name === 'LocationSearchError') {
+        if (response.output.statusCode === INTERNAL_SERVER_ERROR && response.name === 'LocationSearchError') {
           view = 'location-error'
           statusCode = response.output.statusCode
           logLevel = 'error'
           viewModel.pageTitle = 'Sorry, there is a problem with the search - Check for flooding'
-        } else if (response.output.statusCode === 429) {
+        } else if (response.output.statusCode === HTTP_TOO_MANY_REQUESTS) {
           view = '429'
           logLevel = 'warn'
           statusCode = response.output.statusCode
-        } else if (response.output.statusCode === HTTP_NOT_FOUND || (response.output.statusCode === 400 && response.message === 'Invalid request params input')) {
+        } else if (response.output.statusCode === HTTP_NOT_FOUND || (response.output.statusCode === HTTP_BAD_REQUEST && response.message === 'Invalid request params input')) {
           statusCode = 404
           view = '404'
           logLevel = 'debug'
