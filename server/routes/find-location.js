b805c90245905d145cbe9f85b0b319c6da23d7a1
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2024-03-13 16:08:48 +0000

    SonarCloud: removed magic number

--- server/routes/find-location.js
+++ server/routes/find-location.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const HTTP_MOVED_PERMANENTLY = 301
+
 module.exports = [{
   method: 'GET',
   path: '/find-location',
   // note: this handler can be removed once the redirect is added at the infrastructure level
-  handler: async (_request, h) => h.redirect('/').code(301)
+  handler: async (_request, h) => h.redirect('/').code(HTTP_MOVED_PERMANENTLY)
 
 }]
