1f90f323c7ed244ae433e16e5ff4c6b1eb820f7e
Keyurx11 <keyurx11@gmail.com>
2024-04-03 11:20:58 +0100

    Update related content, revise alert banner links, remove unused views/routes

--- test/routes/index.js
+++ test/routes/index.js
@@ -70,54 +70,6 @@ lab.experiment('Get Routes test', () => {
     Code.expect(response.payload).to.include('This phone number has been automatically opted-in to receive flood warnings')
     Code.expect(response.headers['content-type']).to.include('text/html')
   })
-  lab.test('GET /what-to-do-in-a-flood', async () => {
-    const plugin = {
-      plugin: {
-        name: 'what-to-do-in-a-flood',
-        register: (server, options) => {
-          server.route(require('../../server/routes/what-to-do-in-a-flood'))
-        }
-      }
-    }
-
-    await server.register(require('../../server/plugins/views'))
-    await server.register(plugin)
-    await server.initialize()
-
-    const options = {
-      method: 'GET',
-      url: '/what-to-do-in-a-flood'
-    }
-
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.payload).to.include('What the flood warnings mean')
-    Code.expect(response.headers['content-type']).to.include('text/html')
-  })
-  lab.test('GET /plan-ahead-for-flooding', async () => {
-    const plugin = {
-      plugin: {
-        name: 'plan-ahead-for-flooding',
-        register: (server, options) => {
-          server.route(require('../../server/routes/plan-ahead-for-flooding'))
-        }
-      }
-    }
-
-    await server.register(require('../../server/plugins/views'))
-    await server.register(plugin)
-    await server.initialize()
-
-    const options = {
-      method: 'GET',
-      url: '/plan-ahead-for-flooding'
-    }
-
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.payload).to.include('How to plan ahead for flooding')
-    Code.expect(response.headers['content-type']).to.include('text/html')
-  })
   lab.test('GET /what-happens-after-a-flood', async () => {
     const plugin = {
       plugin: {
@@ -142,28 +94,28 @@ lab.experiment('Get Routes test', () => {
     Code.expect(response.payload).to.include('What happens after a flood')
     Code.expect(response.headers['content-type']).to.include('text/html')
   })
-  lab.test('GET /recovering-after-a-flood', async () => {
+  lab.test('GET /find-location', async () => {
     const plugin = {
       plugin: {
-        name: 'recovering-after-a-flood',
+        name: 'find-location',
         register: (server, options) => {
-          server.route(require('../../server/routes/recovering-after-a-flood'))
+          server.route(require('../../server/routes/find-location'))
         }
       }
     }
 
     await server.register(require('../../server/plugins/views'))
+    await server.register(require('../../server/plugins/session'))
     await server.register(plugin)
     await server.initialize()
 
     const options = {
       method: 'GET',
-      url: '/recovering-after-a-flood'
+      url: '/find-location'
     }
 
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.payload).to.include('How to recover after a flood')
     Code.expect(response.headers['content-type']).to.include('text/html')
   })
   lab.test('GET /cookies', async () => {
