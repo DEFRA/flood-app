bcb12d80d7541da58520948d42d303ba4f036a52
Keyurx11 <keyurx11@gmail.com>
2024-04-11 15:27:22 +0100

    Change data-journey-click strings to boolean on target area page

--- server/models/views/target-area.js
+++ server/models/views/target-area.js
@@ -75,12 +75,8 @@ class ViewModel {
       mapTitle,
       floodRiskUrl,
       bingMaps: bingKeyMaps,
-      signUpForFloodWarnings: 'Target Area:Get Warnings:TA - Get warnings',
-      planAhead: 'Target Area:Related-content:Plan-ahead-for-flooding',
-      whatToDo: 'Target Area:Related-content:What-to-do-in-a-flood',
-      recoverAfter: 'Target Area:Related-content:Recover-after-a-flood',
-      longTerm: 'Station:Related-content:Check-long-term-risk',
-      reportFlood: 'Target Area:Related-content:Report-a-flood'
+      signUpForFloodWarnings: true,
+      longTerm: true
     }, options)
   }
 }
