e50c6223c0258d438435ff24a1e0af5a95a63de1
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2024-04-15 08:45:34 +0100

    changed condition of parentAlertArea

--- server/models/views/target-area.js
+++ server/models/views/target-area.js
@@ -45,8 +45,7 @@ class ViewModel {
     const description = area.description.endsWith('.') ? area.description.slice(0, -1) : area.description
     const areaDescription = `Flood ${type} area: ${description}.`
 
-    const parentAreaAlert = (!!(((flood && severityLevel.id === 4) && (type === 'warning')) || !flood) && (parentSeverityLevel.isActive))
-
+    const parentAreaAlert = (!!(((flood && severityLevel.id === 4) && (type === 'warning')) || !flood) && (parentSeverityLevel && parentSeverityLevel.isActive))
     let situationChanged = flood
       ? `Updated ${timeSituationChanged} on ${dateSituationChanged}`
       : `Up to date as of ${timeSituationChanged} on ${dateSituationChanged}`
