23348fa9be7906fec641dc0c73a992452a7d75db
Keyurx11 <keyurx11@gmail.com>
2024-04-26 14:22:08 +0100

    Create helper function to reduce code repetition

---
+++ test/lib/helpers/context-footer-checker.js
@@ -0,0 +1,25 @@
+const { parse } = require('node-html-parser')
+const { expect } = require('@hapi/code')
+
+function validateFooterContent (response, config) {
+  const root = parse(response.payload)
+  const asideElement = root.querySelector('aside.defra-context-footer')
+  const contextFooterHTML = asideElement ? asideElement.innerHTML : ''
+
+  expect(contextFooterHTML).to.contain('<header class="govuk-heading-m">Call Floodline for advice</header>')
+  expect(contextFooterHTML).to.contain('<strong>Floodine helpline</strong>')
+  expect(contextFooterHTML).to.contain('Telephone: 0345 988 1188')
+  expect(contextFooterHTML).to.contain('Textphone: 0345 602 6340')
+  expect(contextFooterHTML).to.contain('Open 24 hours a day, 7 days a week')
+  expect(contextFooterHTML).to.contain('<a href="https://gov.uk/call-charges">Find out more about call charges</a>')
+
+  // Check for the presence of "Talk to a Floodline adviser over webchat" only if webchat is enabled
+  if (config.webchat.enabled) {
+    expect(contextFooterHTML).to.contain('<strong>Talk to a Floodline adviser over webchat</strong>')
+    expect(contextFooterHTML).to.contain('We\'re running webchat as a trial.')
+  }
+}
+
+module.exports = {
+  validateFooterContent
+}
