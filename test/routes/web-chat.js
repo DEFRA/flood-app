057c197dc853dc9fe7a507cfce61d998f24d1cc7
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2024-05-02 08:01:56 +0100

    Refactor to remove duplication

--- test/routes/web-chat.js
+++ test/routes/web-chat.js
@@ -42,84 +42,62 @@ describe('context-footer', () => {
   beforeEach(async () => {
   })
 
-  it('target area page should display webchat link', async () => {
-    const FakeViewModel = sinon.spy(function (options) {
-      Object.assign(this, {
-        pageTitle: 'Page Title',
-        ...options
+  describe('target area page', () => {
+    beforeEach(async () => {
+      const FakeViewModel = sinon.spy(function (options) {
+        Object.assign(this, {
+          pageTitle: 'Page Title',
+          ...options
+        })
       })
-    })
 
-    const targetAreaRoute = proxyquire('../../server/routes/target-area', {
-      '../../server/models/views/target-area': FakeViewModel
-    })
-    const targetAreaPlugin = {
-      plugin: {
-        name: 'target',
-        register: (server, options) => {
-          server.route(targetAreaRoute)
+      const targetAreaRoute = proxyquire('../../server/routes/target-area', {
+        '../../server/models/views/target-area': FakeViewModel
+      })
+      const targetAreaPlugin = {
+        plugin: {
+          name: 'target',
+          register: (server, options) => {
+            server.route(targetAreaRoute)
+          }
         }
       }
-    }
-    await setupServer([
-      targetAreaPlugin,
-      proxyquire('../../server/plugins/views', {
-        '../../server/config': { webchat: { enabled: true } }
-      }),
-      proxyquire('../../server/plugins/session', {})
-    ])
+      await setupServer([
+        targetAreaPlugin,
+        proxyquire('../../server/plugins/session', {})
+      ])
+      const area = getTargetArea({ code: '011WAFDW' })
 
-    const area = getTargetArea({ code: '011WAFDW' })
-
-    server.method('flood.getFloodArea', sinon.stub().resolves(area))
-    server.method('flood.getFloods', sinon.stub().resolves({ floods: [] }))
-
-    const response = await getResponse('011WAFDW')
-
-    expect(response.statusCode).to.equal(200)
-    const root = parse(response.payload)
-    const text = 'Talk to a Floodline adviser over webchat'
-    const pFound = root.querySelectorAll('p.govuk-\\!-margin-bottom-0').some(p => p.text.trim().startsWith(text))
-    expect(pFound, `p tag with text ${text} not found.`).to.be.true()
-  })
-  it('target area page should not display webchat link', async () => {
-    const FakeViewModel = sinon.spy(function (options) {
-      Object.assign(this, {
-        pageTitle: 'Page Title',
-        ...options
-      })
+      server.method('flood.getFloodArea', sinon.stub().resolves(area))
+      server.method('flood.getFloods', sinon.stub().resolves({ floods: [] }))
     })
+    it('should display webchat link', async () => {
+      await server.register(
+        proxyquire('../../server/plugins/views', {
+          '../../server/config': { webchat: { enabled: true } }
+        })
+      )
+      const response = await getResponse('011WAFDW')
 
-    const targetAreaRoute = proxyquire('../../server/routes/target-area', {
-      '../../server/models/views/target-area': FakeViewModel
+      expect(response.statusCode).to.equal(200)
+      const root = parse(response.payload)
+      const text = 'Talk to a Floodline adviser over webchat'
+      const pFound = root.querySelectorAll('p.govuk-\\!-margin-bottom-0').some(p => p.text.trim().startsWith(text))
+      expect(pFound, `p tag with text ${text} not found.`).to.be.true()
     })
-    const targetAreaPlugin = {
-      plugin: {
-        name: 'target',
-        register: (server, options) => {
-          server.route(targetAreaRoute)
-        }
-      }
-    }
-    await setupServer([
-      targetAreaPlugin,
-      proxyquire('../../server/plugins/views', {
-        '../../server/config': { webchat: { enabled: false } }
-      }),
-      proxyquire('../../server/plugins/session', {})
-    ])
+    it('should not display webchat link', async () => {
+      await server.register(
+        proxyquire('../../server/plugins/views', {
+          '../../server/config': { webchat: { enabled: false } }
+        })
+      )
+      const response = await getResponse('011WAFDW')
 
-    const area = getTargetArea({ code: '011WAFDW' })
-
-    server.method('flood.getFloodArea', sinon.stub().resolves(area))
-    server.method('flood.getFloods', sinon.stub().resolves({ floods: [] }))
-
-    const response = await getResponse('011WAFDW')
-
-    expect(response.statusCode).to.equal(200)
-    const root = parse(response.payload)
-    const text = 'Talk to a Floodline adviser over webchat'
-    const pFound = root.querySelectorAll('p.govuk-\\!-margin-bottom-0').some(p => p.text.trim().startsWith(text))
-    expect(pFound, `p tag with text ${text} found.`).to.be.false()
+      expect(response.statusCode).to.equal(200)
+      const root = parse(response.payload)
+      const text = 'Talk to a Floodline adviser over webchat'
+      const pFound = root.querySelectorAll('p.govuk-\\!-margin-bottom-0').some(p => p.text.trim().startsWith(text))
+      expect(pFound, `p tag with text ${text} found.`).to.be.false()
+    })
   })
 })
