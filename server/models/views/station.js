3aac7847e0659583d3156c8767fa1766dea11e56
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2024-06-11 16:18:28 +0100

    swap forecast check to display time series table check

--- server/models/views/station.js
+++ server/models/views/station.js
@@ -20,6 +20,7 @@ class ViewModel {
     this.station.riverNavigation = river
     this.id = station.id
     this.river = river
+    const isForecast = forecast ? forecast.forecastFlag.display_time_series : false
 
     this.station.trend = river.trend
 
@@ -365,8 +366,7 @@ class ViewModel {
 
     let forecastData
     if (forecast) {
-      const { thresholds } = forecast
-      this.isFfoi = thresholds.length > 0
+      this.isFfoi = isForecast
       if (this.isFfoi) {
         forecastData = new Forecast(forecast, this.station.isCoastal, this.station.recentValue)
         this.isForecast = forecastData.hasForecastData
