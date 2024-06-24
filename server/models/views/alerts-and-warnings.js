23a382853f4cc2d1618d2aa1e63ee66a311f44aa
Ash <ashley@digicodeltd.com>
2024-06-24 15:47:54 +0100

    FSR-1207 | update page title to use place name

--- server/models/views/alerts-and-warnings.js
+++ server/models/views/alerts-and-warnings.js
@@ -21,13 +21,15 @@ class ViewModel {
       floodRiskUrl: config.floodRiskUrl
     })
 
+    const pageTitle = place?.name ?? location
+
     if (error) {
       this.pageTitle = 'Sorry, there is currently a problem searching a location'
     } else {
       if (this.station && this.station.agency_name) {
         this.pageTitle = `${this.station.agency_name} - flood alerts and warnings`
       } else {
-        this.pageTitle = `${location ? location + ' - f' : 'F'}lood alerts and warnings`
+        this.pageTitle = `${pageTitle ? pageTitle + ' - f' : 'F'}lood alerts and warnings`
       }
     }
     this.countFloods = floods?.floods?.length ?? 0
