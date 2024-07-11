8048a90c796eb5d8b19176720a9243cefe7fb9d3
Ash <ashleyjtaylor@users.noreply.github.com>
2024-07-11 13:27:31 +0100

    FSR-1208 | river and sea levels search redirects (#723)
    * FSR-1208 | river and sea levels search redirects
    
    * FSR-1208 | river and sea levels search redirects
    
    * sonar fixes 1
    
    * sonar fixes 2
    
    * sonar fixes 3
    
    * move duplicate functions to lib/utils
    
    * sonar fix
    
    * sonar fixes
    
    * sonar fix undo
    
    * add util funcitons
    
    * remove commented code
    
    * remove locationHandler fn
    
    * Refactor locationRouteHandler in river-and-sea-levels.js
    
    * Refactor locationRouteHandler else clause
    
    * remove comment and use util func
    
    * redirect on non-england locations
    
    * remove uneeded test
    
    * add fakeGetJson to test
    
    * sonar fix
    
    * fix response behaviour
    
    * fix single character search result
    
    * remove flakey test
    
    * refactored tests to be inline with alerts-and-warnings
    
    * test coverage
    
    * typo
    
    * test coverage
    
    * add script encode test
    
    ---------
    
    Co-authored-by: Ash <ashley@digicodeltd.com>
    Co-authored-by: nikiwycherley <niki.wycherley@environment-agency.gov.uk>

--- test/routes/alerts-and-warnings.js
+++ test/routes/alerts-and-warnings.js
@@ -149,7 +149,8 @@ lab.experiment('Test - /alerts-warnings', () => {
 
     const response = await server.inject(options)
 
-    Code.expect(response.statusCode).to.equal(404)
+    Code.expect(response.statusCode).to.equal(302)
+    Code.expect(response.headers.location).to.equal('/alerts-and-warnings')
   })
 
   lab.test('GET /alerts-and-warnings with legacy query parameter valid non-england', async () => {
@@ -164,6 +165,22 @@ lab.experiment('Test - /alerts-warnings', () => {
 
     Code.expect(response.statusCode).to.equal(301)
     Code.expect(response.headers.location).to.equal('/alerts-and-warnings/kinghorn-fife')
+  })
+
+  lab.test('GET /alerts-and-warnings with legacy query parameter invalid characters', async () => {
+    stubs.getJson.callsFake(() => data.warringtonGetJson)
+    stubs.getIsEngland.callsFake(() => ({ is_england: true }))
+    stubs.getFloodsWithin.callsFake(() => ({ floods: [] }))
+
+    const options = {
+      method: 'GET',
+      url: '/alerts-and-warnings?q=warrington%*_'
+    }
+
+    const response = await server.inject(options)
+
+    Code.expect(response.statusCode).to.equal(301)
+    Code.expect(response.headers.location).to.equal('/alerts-and-warnings/warrington')
   })
 
   lab.test('GET /alerts-and-warnings/{location} with location', async () => {
@@ -310,6 +327,24 @@ lab.experiment('Test - /alerts-warnings', () => {
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(301)
     Code.expect(response.headers['content-type']).to.include('text/html')
+  })
+
+  lab.test('POST /alerts-and-warnings with location england should show location not found', async () => {
+    stubs.getJson.callsFake(() => data.nonLocationGetJson)
+    stubs.getIsEngland.callsFake(() => ({ is_england: true }))
+    stubs.getFloodsWithin.callsFake(() => [])
+
+    const options = {
+      method: 'POST',
+      url: '/alerts-and-warnings',
+      payload: {
+        location: 'england'
+      }
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(302)
+    Code.expect(response.headers.location).to.equal('/alerts-and-warnings')
   })
 
   lab.test('POST /alerts-and-warnings with invalid payload', async () => {
