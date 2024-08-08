932c2ceb56a2c124bc69106bd44efef83767c3fc
Keyurx11 <keyurx11@gmail.com>
2024-08-08 14:03:25 +0100

    FSR-1237: Updated to latest requirements dont show over 4 warning levels

--- server/models/views/lib/latest-levels.js
+++ server/models/views/lib/latest-levels.js
@@ -3,26 +3,24 @@ const { formatElapsedTime } = require('../../../util')
 const WARNING_THRESHOLD_TYPES = ['FW RES FW', 'FW ACT FW', 'FW ACTCON FW']
 
 function getThresholdsForTargetArea (thresholds) {
-  const warningThreshold = findMinimumThreshold(thresholds, WARNING_THRESHOLD_TYPES)
-  return {
-    warning: formatThresholdTimestamp(warningThreshold)
-  }
+  const warningThresholds = findPrioritizedThresholds(thresholds, WARNING_THRESHOLD_TYPES)
+  return warningThresholds.map(formatThresholdTimestamp)
 }
 
-function findMinimumThreshold (thresholds, types) {
-  let selectedThreshold = null
+function findPrioritizedThresholds (thresholds, types) {
+  const thresholdMap = new Map()
 
   for (const type of types) {
     const filteredThresholds = thresholds.filter(threshold => threshold.threshold_type === type)
-    if (filteredThresholds.length > 0) {
-      selectedThreshold = filteredThresholds.reduce((min, current) => {
-        return (min === null || parseFloat(current.value) < parseFloat(min.value)) ? current : min
-      }, selectedThreshold)
-    }
-    if (selectedThreshold) break
+
+    filteredThresholds.forEach(threshold => {
+      if (!thresholdMap.has(threshold.rloi_id)) {
+        thresholdMap.set(threshold.rloi_id, threshold)
+      }
+    })
   }
 
-  return selectedThreshold
+  return Array.from(thresholdMap.values())
 }
 
 function formatThresholdTimestamp (threshold) {
