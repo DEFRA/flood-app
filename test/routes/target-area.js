2eaaf30d2abef8a3b47820b687f841ac4ed3e035
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2024-08-12 14:43:17 +0100

    FSR-1237: Add unit tests for latest levels box

--- test/routes/target-area.js
+++ test/routes/target-area.js
@@ -4,9 +4,12 @@ const Hapi = require('@hapi/hapi')
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const sinon = require('sinon')
+const moment = require('moment-timezone')
 const lab = exports.lab = Lab.script()
 const { parse } = require('node-html-parser')
 const fakeTargetAreaFloodData = require('../data/fakeTargetAreaFloodData.json')
+const fakeTaThresholds = require('../data/taThreshold.json')
+const fakeTaThresholdsOverLimit = require('../data/taThresholdsOverLimit.json')
 const { linkChecker } = require('../lib/helpers/html-expectations')
 const { validateFooterPresent } = require('../lib/helpers/context-footer-checker')
 
@@ -123,11 +126,22 @@ lab.experiment('Target-area tests', () => {
       url: '/target-area/011WAFDW'
     }
 
+    const fixedTime = moment.tz('2024-08-08T18:15:00.000Z', 'Europe/London')
+    const clock = sinon.useFakeTimers(fixedTime.valueOf())
+
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(200)
+
+    // Latest level (single threshold)
+    Code.expect(response.payload).to.contain('<h2 class="defra-live__title">Latest level</h2>')
+    Code.expect(response.payload).to.contain('<p>The River Derwent level at Derby St Marys was 0.54 metres. Property flooding is possible when it goes above 3.30 metres.')
+    Code.expect(response.payload).to.contain('<a href="/station/2138">Monitor the latest level at Derby St Marys</a>')
     Code.expect(response.payload).to.match(/<div class="defra-flood-status-item__text">\s*<strong>Flooding is possible - <a class="govuk-link" href="https:\/\/www\.gov\.uk\/guidance\/flood-alerts-and-warnings-what-they-are-and-what-to-do#flood-alert">\s*be prepared\s*<\/a><\/strong>\s*<\/div>/)
+    Code.expect(response.payload).to.contain('<p class="defra-flood-meta defra-flood-meta--no-border govuk-!-margin-bottom-0"><strong>30 minutes ago</strong></p>')
+
+    clock.restore()
   })
-  lab.test('Check flood severity banner link for Flood warning', async () => {
+  lab.test('Check flood severity banner link for Flood warning and multi latest levels', async () => {
     const floodService = require('../../server/services/flood')
 
     const fakeFloodData = () => {
@@ -143,7 +157,7 @@ lab.experiment('Target-area tests', () => {
       return fakeTargetAreaFloodData.area
     }
 
-    const fakeTAThresholdsData = () => [{ station_threshold_id: '1746074', station_id: '2138', fwis_code: '034FWFDEDERWATER', fwis_type: 'W', direction: 'u', value: '0.535', threshold_type: 'FW RES FW', river_id: 'river-derwent-derbyshire', river_name: 'River Derwent', river_qualified_name: 'River Derwent (Derbyshire)', navigable: true, view_rank: 1, rank: '5', rloi_id: 2138, up: 2103, down: 2130, telemetry_id: '4085', region: 'Midlands', catchment: 'Derbyshire Derwent', wiski_river_name: 'River Derwent', agency_name: 'Derby St Marys', external_name: 'Derby City', station_type: 'S', status: 'Active', qualifier: 'u', iswales: false, value_timestamp: '2024-08-08T17:45:00.000Z', value_erred: false, trend: 'steady', percentile_5: '1.85', percentile_95: '0.583', centroid: '0101000020E61000004B9C1B3B719BF7BFF845A5CDB0764A40', lon: -1.4754497822666675, lat: 52.92727060861574, day_total: null, six_hr_total: null, one_hr_total: null, id: '2114', threshold_value: '3.3', latest_level: '0.535' }]
+    const fakeTAThresholdsData = () => fakeTaThresholds
 
     sandbox.stub(floodService, 'getTargetAreaThresholds').callsFake(fakeTAThresholdsData)
     sandbox.stub(floodService, 'getFloods').callsFake(fakeFloodData)
@@ -174,6 +188,15 @@ lab.experiment('Target-area tests', () => {
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(200)
     Code.expect(response.payload).to.match(/<div class="defra-flood-status-item__text">\s*<strong>Flooding is expected - <a class="govuk-link" href="https:\/\/www\.gov\.uk\/guidance\/flood-alerts-and-warnings-what-they-are-and-what-to-do#flood-warning">\s*act now\s*<\/a><\/strong>\s*<\/div>/)
+
+    // latests levels (multi)
+    Code.expect(response.payload).to.contain('<h2 class="defra-live__title">Latest levels</h2>')
+    Code.expect(response.payload).to.contain('<p>The River Pinn level at Eastcote Road was 0.35 metres. Property flooding is possible when it goes above 1.40 metres.')
+    Code.expect(response.payload).to.contain('<p>The River Pinn level at Avenue Road was 0.18 metres. Property flooding is possible when it goes above 1.46 metres.')
+    Code.expect(response.payload).to.contain(' <a href="/station/7173">Monitor the River Pinn level at Avenue Road</a>')
+    Code.expect(response.payload).to.contain('<p>The River Pinn level at Moss Close was 0.13 metres. Property flooding is possible when it goes above 1.15 metres.')
+    Code.expect(response.payload).to.contain('<a href="/station/7201">Monitor the River Pinn level at Moss Close</a>')
+    Code.expect(response.payload).to.contain('<p class="defra-live__supplementary">These levels will update automatically</p>')
   })
   lab.test('Check flood severity banner link for Flood warning', async () => {
     const floodService = require('../../server/services/flood')
@@ -395,5 +418,101 @@ lab.experiment('Target-area tests', () => {
     // context footer check
     validateFooterPresent(response)
     Code.expect(response.payload).to.contain('There are no flood warnings in this area, but there is <a href="/target-area/123WAF984">a flood alert in the wider area</a>')
+  })
+  lab.test('Should not display latest level, TA has non in DB', async () => {
+    const floodService = require('../../server/services/flood')
+
+    const fakeFloodData = () => {
+      return {
+        floods: fakeTargetAreaFloodData.floods
+      }
+    }
+
+    const fakeTAThresholdsData = () => []
+
+    const fakeFloodArea = () => {
+      return fakeTargetAreaFloodData.area
+    }
+
+    sandbox.stub(floodService, 'getFloods').callsFake(fakeFloodData)
+    sandbox.stub(floodService, 'getFloodArea').callsFake(fakeFloodArea)
+    sandbox.stub(floodService, 'getTargetAreaThresholds').callsFake(fakeTAThresholdsData)
+
+    const targetAreaPlugin = {
+      plugin: {
+        name: 'target',
+        register: (server, options) => {
+          server.route(require('../../server/routes/target-area'))
+        }
+      }
+    }
+
+    await server.register(require('../../server/plugins/views'))
+    await server.register(require('../../server/plugins/session'))
+    await server.register(targetAreaPlugin)
+
+    const registerServerMethods = require('../../server/services/server-methods')
+    registerServerMethods(server)
+
+    await server.initialize()
+    const options = {
+      method: 'GET',
+      url: '/target-area/011WAFDW'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.payload).to.not.contain('<h2 class="defra-live__title">Latest level</h2>')
+  })
+  lab.test('Should not display latest levels as more than four', async () => {
+    const floodService = require('../../server/services/flood')
+
+    const fakeFloodData = () => {
+      fakeTargetAreaFloodData.floods[0].severity_value = 2
+      fakeTargetAreaFloodData.floods[0].severity = 'Flood warning'
+
+      return {
+        floods: fakeTargetAreaFloodData.floods
+      }
+    }
+
+    const fakeFloodArea = () => {
+      return fakeTargetAreaFloodData.area
+    }
+
+    const fakeTAThresholdsData = () => fakeTaThresholdsOverLimit
+
+    sandbox.stub(floodService, 'getTargetAreaThresholds').callsFake(fakeTAThresholdsData)
+    sandbox.stub(floodService, 'getFloods').callsFake(fakeFloodData)
+    sandbox.stub(floodService, 'getFloodArea').callsFake(fakeFloodArea)
+
+    const targetAreaPlugin = {
+      plugin: {
+        name: 'target',
+        register: (server, options) => {
+          server.route(require('../../server/routes/target-area'))
+        }
+      }
+    }
+
+    await server.register(require('../../server/plugins/views'))
+    await server.register(require('../../server/plugins/session'))
+    await server.register(targetAreaPlugin)
+
+    const registerServerMethods = require('../../server/services/server-methods')
+    registerServerMethods(server)
+
+    await server.initialize()
+    const options = {
+      method: 'GET',
+      url: '/target-area/011WAFDW'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.payload).to.match(/<div class="defra-flood-status-item__text">\s*<strong>Flooding is expected - <a class="govuk-link" href="https:\/\/www\.gov\.uk\/guidance\/flood-alerts-and-warnings-what-they-are-and-what-to-do#flood-warning">\s*act now\s*<\/a><\/strong>\s*<\/div>/)
+
+    // latests levels (multi)
+    Code.expect(response.payload).to.not.contain('<h2 class="defra-live__title">Latest levels</h2>')
   })
 })
