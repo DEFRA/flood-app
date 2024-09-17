f25249da01631dadd467e58765b333a531c710bf
keyurx11 <keyurx11@gmail.com>
2024-09-17 17:27:44 +0100

    FSR-1237: Display '(National Resources Wales)' for Welsh stations in flood warnings and add test

--- test/routes/target-area.js
+++ test/routes/target-area.js
@@ -192,7 +192,7 @@ lab.experiment('Target-area tests', () => {
     Code.expect(response.payload).to.contain('<h2 class="defra-live__title">Latest levels</h2>')
     Code.expect(response.payload).to.contain('<p>The River Pinn level at Eastcote Road was 0.35 metres. Property flooding is possible when it goes above 1.40 metres.')
     Code.expect(response.payload).to.contain('<p>The River Pinn level at Avenue Road was 0.18 metres. Property flooding is possible when it goes above 1.46 metres.')
-    Code.expect(response.payload).to.contain(' <a href="/station/7173">Monitor the River Pinn level at Avenue Road</a>')
+    Code.expect(response.payload).to.contain('<a href="/station/7173">Monitor the River Pinn level at Avenue Road</a>')
     Code.expect(response.payload).to.contain('<p>The River Pinn level at Moss Close was 0.13 metres. Property flooding is possible when it goes above 1.15 metres.')
     Code.expect(response.payload).to.contain('<a href="/station/7201">Monitor the River Pinn level at Moss Close</a>')
   })
@@ -950,5 +950,6 @@ lab.experiment('Target-area tests', () => {
 
     Code.expect(response.payload).to.contain('<h2 class="defra-live__title">Latest level</h2>')
     Code.expect(response.payload).to.contain('<p>The River Welsh level at Welsh Station was 0.35 metres. Property flooding is possible when it goes above 1.40 metres.\n        \n      </p>')
+    Code.expect(response.payload).to.contain('<a href="/station/7201">Monitor the latest level at Welsh Station (Natural Resources Wales)</a>')
   })
 })
