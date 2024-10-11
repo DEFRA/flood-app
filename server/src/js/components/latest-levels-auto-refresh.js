c45cdd1a31c34688be57c39b6e0ead45af2c588b
Ash <ashley@digicodeltd.com>
2024-10-11 11:26:13 +0100

    remove console logs

--- server/src/js/components/latest-levels-auto-refresh.js
+++ server/src/js/components/latest-levels-auto-refresh.js
@@ -29,8 +29,6 @@ class LatestLevelsAutoRefresh {
   }
 
   updateTimeAgo = () => {
-    console.log('--updateTimeAgo() started')
-
     setTimeout(() => {
       this.renderTimeAgo()
       this.timeAgoInterval = setInterval(this.renderTimeAgo, 60000)
@@ -40,23 +38,17 @@ class LatestLevelsAutoRefresh {
   renderTimeAgo = () => {
     const elements = document.querySelectorAll('[data-item-time]')
 
-    elements.forEach((element, index) => {
-      console.log('element', index)
+    elements.forEach(element => {
       const timeAgoText = element.textContent
       const timeAgoValue = parseInt(timeAgoText, 10)
 
-      console.log('--currentTime', timeAgoText)
-
       if (!timeAgoText.includes('hour')) {
         element.textContent = `${timeAgoValue + 1} minutes ago`
-        console.log('--newTime', element.textContent)
       }
     })
   }
 
   fetchRiverLevels = (callback) => {
-    console.log('fetchRiverLevels...')
-
     this.liveStatusMessages = []
 
     fetch(window.location.href)
@@ -75,8 +67,7 @@ class LatestLevelsAutoRefresh {
         }
       })
       .catch(error => {
-        console.error('Error updating levels:', error)
-
+        console.error(error)
         this.liveStatusMessages.push('There was an error getting the latest level')
       })
       .finally(() => {
@@ -99,9 +90,6 @@ class LatestLevelsAutoRefresh {
 
     // Check if any elements are missing in the fetched data
     const isMissingElements = this.latestLevels.length !== fetchedElements.length
-    console.log('isMissingElements', isMissingElements, this.latestLevels.length, fetchedElements.length)
-    console.log('latestLevels', this.latestLevels)
-    console.log('fetchedElements', fetchedElements)
 
     if (isMissingElements) {
       return this.liveStatusMessages.push('Warnings have been removed, please refresh the page.')
@@ -118,29 +106,19 @@ class LatestLevelsAutoRefresh {
 
       const currentItem = document.querySelector(`[data-item-id="${itemId}"]`)
 
-      console.log('fetchedElement', itemId, itemRiverName, itemRiverAgency)
-
       if (currentItem) {
         const currentTime = currentItem.querySelector('[data-item-time]')
         const currentValue = currentItem.querySelector('[data-item-value]')
         const currentStatus = currentItem.getAttribute('data-item-status')
 
-        console.log('--currentValue', currentValue?.textContent, currentStatus)
-        console.log('--fetchedValue', fetchedValue?.textContent, fetchedStatus)
-        console.log('--requires update?', fetchedValue?.textContent !== currentValue?.textContent)
-
         if (fetchedStatus === currentStatus) {
           if (fetchedValue?.textContent !== currentValue?.textContent) {
-            console.log('--new value fetched')
             clearInterval(this.timeAgoInterval)
-            console.log('--interval cleared')
             currentValue.textContent = fetchedValue.textContent
 
             this.liveStatusMessages.push(`The ${itemRiverName} at ${itemRiverAgency} level was ${fetchedValue.textContent} metres ${fetchedTime.textContent}`)
 
             this.updateTimeAgo()
-          } else {
-            console.log('--no change')
           }
 
           currentTime.textContent = fetchedTime.textContent
@@ -150,8 +128,6 @@ class LatestLevelsAutoRefresh {
       } else {
         this.liveStatusMessages.push('Please refresh the page')
       }
-
-      console.log('-------------------\n')
     })
   }
 
@@ -163,9 +139,6 @@ class LatestLevelsAutoRefresh {
 
     // Find the next target minute
     const nextTargetMinute = this.targetMinutes.find(minute => minute > nowMinute) ?? this.targetMinutes[0]
-
-    console.log('--Current minute:', nowMinute)
-    console.log('--Next target minute:', nextTargetMinute)
 
     // Create the next target date based on the next target minute
     const nextTargetDate = new Date(now)
@@ -179,9 +152,6 @@ class LatestLevelsAutoRefresh {
 
     // Calculate delay correctly from now
     const delay = nextTargetDate.getTime() - now.getTime()
-
-    console.log('--delay (calc)', Math.round(delay / 1000 / 60))
-    console.log(`Next update scheduled in ${Math.round(delay / 1000 / 60)} minutes at ${nextTargetDate.toLocaleTimeString()}\n\n`)
 
     // Schedule the next update
     this.timeout = setTimeout(() => {
