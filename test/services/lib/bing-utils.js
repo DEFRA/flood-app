9df729b72a36680d315c0dc17d46c554fc87e1f1
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2025-02-06 13:39:56 +0000

    Add tests for slugify
    Also switched from test to it for consistency

--- test/services/lib/bing-utils.js
+++ test/services/lib/bing-utils.js
@@ -1,32 +1,43 @@
 const Lab = require('@hapi/lab')
 const { expect } = require('@hapi/code')
-const { experiment, test } = exports.lab = Lab.script()
-const { formatName } = require('../../../server/services/lib/bing-utils')
+const { experiment, it } = exports.lab = Lab.script()
+const { formatName, slugify } = require('../../../server/services/lib/bing-utils')
 
 experiment('bing utils', () => {
   experiment('formatName', () => {
-    test('Repeating parts are removed', async () => {
+    it('Repeating parts are removed', async () => {
       expect(formatName('Middlesbrough, Middlesbrough')).to.equal('Middlesbrough')
     })
-    test('Similar parts are not removed', async () => {
+    it('Similar parts are not removed', async () => {
       expect(formatName('Durham, County Durham')).to.equal('Durham, County Durham')
     })
-    test('City qualifier "City Of" is removed', async () => {
+    it('City qualifier "City Of" is removed', async () => {
       expect(formatName('Bristol, City Of Bristol')).to.equal('Bristol')
     })
-    test('City qualifier "City of" is removed (ie case insensitive)', async () => {
+    it('City qualifier "City of" is removed (ie case insensitive)', async () => {
       expect(formatName('Bristol, City of Bristol')).to.equal('Bristol')
     })
-    test('City qualifier "Greater" is removed', async () => {
+    it('City qualifier "Greater" is removed', async () => {
       expect(formatName('London, Greater London')).to.equal('London')
     })
-    test('City qualifier removed from neighbourhood', async () => {
+    it('City qualifier removed from neighbourhood', async () => {
       expect(formatName('Camberwell, London, Greater London')).to.equal('Camberwell, London')
     })
     experiment('Error handling', () => {
-      test('Fails gracefully with undefined name', async () => {
+      it('Fails gracefully with undefined name', async () => {
         expect(formatName()).to.equal('')
       })
+    })
+  })
+  experiment('slugify', () => {
+    it('Simple name is slugified', async () => {
+      expect(slugify('Leeds, West Yorkshire')).to.equal('leeds-west-yorkshire')
+    })
+    it('Non-alphanumerics (!) are preserved', async () => {
+      expect(slugify('Westward Ho!, Bideford, Devon')).to.equal('westward-ho!-bideford-devon')
+    })
+    it('Non-alphanumerics (\') are preserved', async () => {
+      expect(slugify('Bishop\'s Stortford')).to.equal('bishop\'s-stortford')
     })
   })
 })
