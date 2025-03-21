549adc6514c8d66f51175716886006b767ebce1a
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2025-03-21 12:12:09 +0000

    Switch to using process exit after due consideration
    This is a fatal startup condition which needs to be communicated with
    external systems

--- server/lib/node-version-check.js
+++ server/lib/node-version-check.js
@@ -2,7 +2,10 @@ const semver = require('semver')
 
 function nodeVersionCheck (requiredVersion) {
   if (requiredVersion && !semver.satisfies(process.version, requiredVersion)) {
-    throw new Error(`Node.js version ${process.version} does not satisfy the required version ${requiredVersion}`)
+    // note - use console.error here rather than pino as this check will be run
+    // before configuration is done
+    console.error(`Error: Node.js version ${process.version} does not satisfy the required version ${requiredVersion}`)
+    process.exit(1)
   }
 }
 
--- test/server/node-version-check.js
+++ test/server/node-version-check.js
@@ -1,14 +1,17 @@
 'use strict'
 
 const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
 const Sinon = require('sinon')
 const Proxyquire = require('proxyquire')
 
-const { expect } = require('@hapi/code')
 const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
 
 describe('Node Version Check', () => {
   let mockSemver
+  let consoleErrorStub
+  let processExitStub
   let nodeVersionCheck
   let originalProcessVersion
   const testNodeVersion = 'v20.18.2'
@@ -16,14 +19,20 @@ describe('Node Version Check', () => {
   beforeEach(() => {
     originalProcessVersion = process.version
 
+    // need to do this rather than just process.version = '1.1.1' as the
+    // property is reaad only
     Object.defineProperty(process, 'version', {
       value: testNodeVersion,
       configurable: true
     })
 
+    consoleErrorStub = Sinon.stub(console, 'error')
+
     mockSemver = {
       satisfies: Sinon.stub()
     }
+
+    processExitStub = Sinon.stub(process, 'exit')
 
     nodeVersionCheck = Proxyquire('../../server/lib/node-version-check', {
       semver: mockSemver
@@ -35,43 +44,50 @@ describe('Node Version Check', () => {
       value: originalProcessVersion,
       configurable: true
     })
+
+    processExitStub.restore()
+    consoleErrorStub.restore()
   })
 
   describe('nodeVersionCheck', () => {
-    it('should not throw when no version requirement is provided', () => {
-      expect(() => nodeVersionCheck()).to.not.throw()
-      expect(() => nodeVersionCheck(null)).to.not.throw()
-      expect(() => nodeVersionCheck(undefined)).to.not.throw()
+    it('should not exit when no version requirement is provided', () => {
+      nodeVersionCheck()
+      nodeVersionCheck(null)
+      nodeVersionCheck(undefined)
 
       expect(mockSemver.satisfies.called).to.be.false()
+      expect(processExitStub.called).to.be.false()
     })
 
-    it('should not throw when node version satisfies requirement', () => {
+    it('should not exit when node version satisfies requirement', () => {
       mockSemver.satisfies.returns(true)
       const requiredVersion = '20.x'
 
-      expect(() => nodeVersionCheck(requiredVersion)).to.not.throw()
+      nodeVersionCheck(requiredVersion)
 
       expect(mockSemver.satisfies.calledOnceWith(testNodeVersion, requiredVersion)).to.be.true()
+      expect(processExitStub.called).to.be.false()
     })
 
-    it('should throw when node version does not satisfy requirement', () => {
+    it('should exit with code 1 when node version does not satisfy requirement', () => {
       mockSemver.satisfies.returns(false)
       const requiredVersion = '21.x'
 
-      expect(() => nodeVersionCheck(requiredVersion)).to.throw(Error)
+      nodeVersionCheck(requiredVersion)
 
       expect(mockSemver.satisfies.calledOnceWith(testNodeVersion, requiredVersion)).to.be.true()
+      expect(processExitStub.calledOnceWith(1)).to.be.true()
     })
 
-    it('should include version information in error message', () => {
+    it('should log error message with version information when check fails', () => {
       mockSemver.satisfies.returns(false)
       const requiredVersion = '21.x'
 
-      const error = expect(() => nodeVersionCheck(requiredVersion)).to.throw(Error)
-      expect(error.message).to.equal('Node.js version v20.18.2 does not satisfy the required version 21.x')
+      nodeVersionCheck(requiredVersion)
 
-      expect(mockSemver.satisfies.calledOnceWith(testNodeVersion, requiredVersion)).to.be.true()
+      expect(consoleErrorStub.calledOnce).to.be.true()
+      const expectedMessage = `Error: Node.js version ${testNodeVersion} does not satisfy the required version ${requiredVersion}`
+      expect(consoleErrorStub.firstCall.args[0]).to.equal(expectedMessage)
     })
   })
 })
