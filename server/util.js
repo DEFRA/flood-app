15229bbcad390222d9bf83ac756d07d246f8700c
Niki Wycherley <niki.wycherley@defra.gov.uk>
2025-04-23 14:50:40 +0100

    more sonar cloud

--- server/util.js
+++ server/util.js
@@ -19,7 +19,7 @@ async function request (method, url, options) {
     payload = response.payload
   } catch (error) {
     if (error?.message?.startsWith('Response Error:')) {
-      error.message += ` on ${method.toUpperCase()} ${url.replace(/\?.*$/, '')}`
+      error.message += ` on ${method.toUpperCase()} ${url.replace(/\?[^]*$/, '')}`
     }
     throw error
   }
