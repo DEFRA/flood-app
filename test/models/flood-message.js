051b1d21029518c965329724fb32260835a3944e
Ash <ashley@digicodeltd.com>
2025-05-20 15:55:40 +0100

    updated test files

--- test/models/flood-message.js
+++ test/models/flood-message.js
@@ -1,35 +1,36 @@
 'use strict'
 
 const Lab = require('@hapi/lab')
-const Code = require('@hapi/code')
-const { expect } = Code
-const { experiment, test } = (exports.lab = Lab.script())
+const { expect } = require('@hapi/code')
+const { describe, it } = (exports.lab = Lab.script())
 const { getSevereWarnings, displayGetWarningsLink } = require('../../server/models/floods-message')
 
-experiment('getSevereWarnings function', () => {
-  test('should return "one severe flood warning" for a single severe warning', () => {
-    expect(getSevereWarnings(1)).to.equal('one severe flood warning')
+describe('Model - Flood Message', () => {
+  describe('getSevereWarnings', () => {
+    it('should return "one severe flood warning" for a single severe warning', () => {
+      expect(getSevereWarnings(1)).to.equal('one severe flood warning')
+    })
+
+    it('should return "0 severe flood warnings" for zero severe warnings', () => {
+      expect(getSevereWarnings(0)).to.equal('0 severe flood warning')
+    })
+
+    it('should return "{n} severe flood warnings" for multiple severe warnings', () => {
+      expect(getSevereWarnings(3)).to.equal('3 severe flood warnings')
+    })
   })
 
-  test('should return "0 severe flood warnings" for zero severe warnings', () => {
-    expect(getSevereWarnings(0)).to.equal('0 severe flood warning')
-  })
+  describe('displayGetWarningsLink', () => {
+    it('should return "one flood warning" for a single warning', () => {
+      expect(displayGetWarningsLink(1)).to.equal('one flood warning')
+    })
 
-  test('should return "{n} severe flood warnings" for multiple severe warnings', () => {
-    expect(getSevereWarnings(3)).to.equal('3 severe flood warnings')
-  })
-})
+    it('should return "0 flood warnings" for zero warnings', () => {
+      expect(displayGetWarningsLink(0)).to.equal('0 flood warning')
+    })
 
-experiment('displayGetWarningsLink function', () => {
-  test('should return "one flood warning" for a single warning', () => {
-    expect(displayGetWarningsLink(1)).to.equal('one flood warning')
-  })
-
-  test('should return "0 flood warnings" for zero warnings', () => {
-    expect(displayGetWarningsLink(0)).to.equal('0 flood warning')
-  })
-
-  test('should return "{n} flood warnings" for multiple warnings', () => {
-    expect(displayGetWarningsLink(2)).to.equal('2 flood warnings')
+    it('should return "{n} flood warnings" for multiple warnings', () => {
+      expect(displayGetWarningsLink(2)).to.equal('2 flood warnings')
+    })
   })
 })
