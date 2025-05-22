188b8ed4cc6f885165a9dbcffae4e443bae7416e
Neil McLaughlin <neil.mclaughlin@defra.gov.uk>
2025-05-22 11:43:31 +0100

    FSR-1386 : fix 404 error which occurs for a small number of towns (#948)
    * FSR-1388: Fix 404 errors for some towns/villages
    
    * Add integration tests for bing
    
    * Refactor parser to use map function in prep for returning >1 result
    
    * Remove unused parameter
    
    * Fix broken tests
    
    * Remove redundant parameter from tests
    
    * Refactor to allow confidence filter to be overriden
    
    * Add the get method for location search to find by slug
    
    * Use shared utility slugify
    
    * Remove default prefilter from parser and set it in the location service
    
    * Fix for locations which have had their names formatted
    
    * Refactor to more specific use cases - find and get
    
    * Extract filter, mapper and sort functions
    
    This is for legibility and reuse reasons
    
    * Use specific implementations for find and get
    
    This is rather than having the general function for both
    
    * Move previously failing test towns/villages into passing list
    
    * Add sample counties/cities
    
    * Move bing results processing utilities to their own file
    
    * Add tests for slugify
    
    Also switched from test to it for consistency
    
    * Add some explanatory comments
    
    * Use different maxBingResults for find and get
    
    see comments in code for background

--- server/services/location.js
+++ server/services/location.js
@@ -2,11 +2,8 @@ const joi = require('joi')
 const { bingKeyLocation, bingUrl } = require('../config')
 const { getJson } = require('../util')
 const util = require('util')
-const bingResultsParser = require('./lib/bing-results-parser')
+const { find, get } = require('./lib/bing-results-parser')
 const LocationSearchError = require('../location-search-error')
-const floodServices = require('./flood')
-
-const MAX_BING_RESULTS = 3
 
 function bingSearchNotNeeded (searchTerm) {
   const mustNotMatch = /[<>]|^england$|^scotland$|^wales$|^united kingdom$|^northern ireland$/i
@@ -35,15 +32,16 @@ function validateBingResponse (response) {
   }
 }
 
-async function find (location) {
-  const validatedLocation = validateSearchTerm(location)
+async function getBingResponse (query, maxBingResults) {
+  const validatedQuery = validateSearchTerm(query)
+  const emptyBingResponse = { resourceSets: [{ estimatedTotal: 0 }] }
 
-  if (bingSearchNotNeeded(validatedLocation)) {
-    return []
+  if (bingSearchNotNeeded(validatedQuery)) {
+    return emptyBingResponse
   }
 
-  const query = encodeURIComponent(validatedLocation)
-  const url = util.format(bingUrl, query, MAX_BING_RESULTS, bingKeyLocation)
+  const encodedQuery = encodeURIComponent(validatedQuery)
+  const url = util.format(bingUrl, encodedQuery, maxBingResults, bingKeyLocation)
 
   let bingData
   try {
@@ -54,7 +52,26 @@ async function find (location) {
 
   validateBingResponse(bingData)
 
-  return bingResultsParser(bingData, floodServices.getIsEngland)
+  return bingData
 }
 
-module.exports = { find }
+async function getLocationBySlug (locationSlug) {
+  // inspection shows that for some slug searches (e.g. hoxne-eye-suffolk)
+  // the desired result is not within the first 3 results so need a different
+  // value. 5 seems to be an acceptable value.
+  const MAX_BING_RESULTS = 5
+
+  const bingData = await getBingResponse(locationSlug, MAX_BING_RESULTS)
+  return get(bingData, locationSlug)
+}
+
+async function findLocationByQuery (locationQuery) {
+  const MAX_BING_RESULTS = 3
+  const bingData = await getBingResponse(locationQuery, MAX_BING_RESULTS)
+  return find(bingData)
+}
+
+module.exports = {
+  find: findLocationByQuery,
+  get: getLocationBySlug
+}
