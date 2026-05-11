3accabbe311e2e0013dfa74bde314437951b1e96
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2026-05-11 16:43:00 +0100

    Additional debug

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -35,7 +35,7 @@ const routes = [].concat(
   require('../routes/what-happens-after-a-flood')
 )
 
-if (JSON.parse(process.env.WEBCHAT_ENABLED || 'false')) {
+if (process.env.WEBCHAT_ENABLED === 'true') {
   routes.push(require('../routes/api/webchat-availability'))
 }
 
