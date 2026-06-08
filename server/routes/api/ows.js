26870416cc838c5da7999eb9c35ea523c8f41596
Niki Wycherley <niki.wycherley@defra.gov.uk>
2026-06-08 10:50:18 +0100

    Add diagnostics for target area OWS requests in ows.js

--- server/routes/api/ows.js
+++ server/routes/api/ows.js
@@ -1,6 +1,11 @@
 const config = require('../../config')
 const uri = config.geoserverUrl + '/geoserver/flood/ows'
 
+const getTargetAreaCodeFromReferrer = (referrer = '') => {
+  const match = referrer.match(/\/target-area\/([^/?#]+)/i)
+  return match ? decodeURIComponent(match[1]) : null
+}
+
 module.exports = {
   method: 'GET',
   path: '/api/ows',
@@ -8,6 +13,21 @@ module.exports = {
     proxy: {
       mapUri: function (request) {
         const url = uri + request.url.search
+        const typename = (request.query.typename || '').toLowerCase()
+
+        // Temporary diagnostics to identify which target-area pages trigger this OWS call.
+        if (typename === 'flood:flood_warning_alert') {
+          const referrer = request.headers.referer || request.headers.referrer || ''
+          const taCode = getTargetAreaCodeFromReferrer(referrer) || 'unknown'
+
+          request.logger.warn({
+            taCode,
+            referrer,
+            endpoint: '/api/ows',
+            typename
+          }, 'Target area OWS request')
+        }
+
         return { uri: url }
       },
       passThrough: true
