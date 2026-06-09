d0aaf84dffb2beafda3b25ed64ab7b9da8f173f7
Niki Wycherley <niki.wycherley@defra.gov.uk>
2026-06-09 15:57:30 +0100

    Add HTTP_MOVED_PERMANENTLY constant and update routes to use it for permanent redirects

--- server/routes/recovering-after-a-flood.js
+++ server/routes/recovering-after-a-flood.js
@@ -1,8 +1,10 @@
+const { HTTP_MOVED_PERMANENTLY } = require('../constants')
+
 module.exports = {
   method: 'GET',
   path: '/recovering-after-a-flood',
   options: {
     description: 'recovering-after-a-flood - Permanent (HTTP 301) redirect to external government guidance page',
-    handler: async (_request, h) => h.redirect('https://www.gov.uk/after-flood').code(301)
+    handler: async (_request, h) => h.redirect('https://www.gov.uk/after-flood').code(HTTP_MOVED_PERMANENTLY)
   }
 }
