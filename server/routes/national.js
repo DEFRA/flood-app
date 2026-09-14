169405c9513098dc7a2dba2a346b2832c5e1c22d
Duncan Abraham <Duncan.Abraham1@defra.gov.uk>
2026-09-14 14:52:36 +0100

    FSR-1667 | Use my location - Outside England (#1220)
    * FSR-1665 | Add use-my-location to the national page
    
    * only show the new button if JS enabled
    
    * added tests to cover coordinate search
    
    * removed unused references
    
    * FSR-1665 | Add use-my-location to the national page
    
    * only show the new button if JS enabled
    
    * added tests to cover coordinate search
    
    * removed unused references
    
    * FSR-1666 | Use my location error reporting
    
    * added GDS errors to the national page for geolocation
    
    * fixed issues arising from PR and quality gates
    
    * fixed structure of national page
    
    * FSR-1667 | use-my-location-outside-england
    
    * fixed issues identified by sonarqube
    
    * fixed incorrect route path and associated tests
    
    * fixed issues highlighted by copilot
    
    * fixed invalid GOV.UK title postfix
    
    * addressed edge case with rural postcode
    
    * merged FSR-1672 | use my location ga tracking
    
    * returned national.html back to its original layout
    
    * removed timeout that only affected Chrome browsers
    
    * fixed issue where text is posted during an error report

--- server/routes/national.js
+++ server/routes/national.js
@@ -1,4 +1,6 @@
 const joi = require('joi')
+
+const { COORDINATE_PATTERN } = require('../constants')
 const OutlookModel = require('../models/outlook')
 const FloodsModel = require('../models/floods')
 const ViewModel = require('../models/views/national')
@@ -21,11 +23,37 @@ async function getModel (request, location) {
   return new ViewModel(floods, outlook, location)
 }
 
+function rejectedLocation (location, geolocation) {
+  return (!geolocation && (location.toLowerCase() === 'england' || location === ''))
+}
+
+function isGeolocationError (location, geolocation, error) {
+  return Boolean(error && !geolocation && location === '')
+}
+
+function getMessageText (geolocationError) {
+  return geolocationError
+    ? 'Turn on location services to use your current location, or enter a town, city or postcode in England'
+    : ''
+}
+
+function getRedirectPath (place, geolocation) {
+  if (geolocation) {
+    return place?.isEngland?.is_england
+      ? '/location/' + encodeURIComponent(place?.slug)
+      : '/outside-england'
+  }
+
+  return place?.isEngland?.is_england
+    ? '/location/' + encodeURIComponent(place?.slug)
+    : null
+}
+
 module.exports = [
   {
     method: 'GET',
     path: '/',
-    handler: async (request, h) => {
+    handler: async function (request, h) {
       const model = await getModel(request)
 
       return h.view('national', { model })
@@ -34,22 +62,35 @@ module.exports = [
   {
     method: 'POST',
     path: '/',
-    handler: async (request, h) => {
-      const { location } = request.payload
-      if (location.toLowerCase() === 'england' || location === '') {
+    handler: async function (request, h) {
+      const { location, error = null, geolocation = null } = request.payload
+      const geolocationError = isGeolocationError(location, geolocation, error)
+      const messageText = getMessageText(geolocationError)
+
+      if (rejectedLocation(location, geolocation)) {
         const model = await getModel(request, location)
+        if (geolocationError) {
+          model.errorMessage = messageText
+          model.pageTitle = `Error: ${messageText}`
+        }
         return h.view('national', { model })
       }
-      const [place] = await locationService.find(location)
-      if (!place?.name || !place.isEngland.is_england) {
-        return h.view('location-not-found', { pageTitle: 'Error: Find location - Check for flooding', location })
+
+      const [place] = await locationService.find(geolocation || location)
+      const redirect = getRedirectPath(place, geolocation)
+
+      if (redirect) {
+        return h.redirect(redirect)
       }
-      return h.redirect(`/location/${encodeURIComponent(place?.slug)}`)
+
+      return h.view('location-not-found', { pageTitle: 'Error: Find location - Check for flooding', location, messageText })
     },
     options: {
       validate: {
         payload: joi.object({
-          location: joi.string().required().trim().allow('')
+          location: joi.string().required().trim().allow(''),
+          geolocation: joi.string().optional().trim().allow('').pattern(COORDINATE_PATTERN),
+          error: joi.string().optional().trim().allow('')
         })
       }
     }
